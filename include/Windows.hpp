c3e55867050902fc8b9b58ec8248c02878c9663d
Owen Green <gungwho@gmail.com>
2018-08-15 12:42:30 +0100

    Make WindowFuncMap static, otherwise cannot include more than once without linker errors

--- include/Windows.hpp
+++ include/Windows.hpp
@@ -15,9 +15,9 @@ using std::function;
 enum class WindowType {Hann, Hamming};
 using WindowFuncMap = map<WindowType, function<vector<double>(int)>>;
 
-WindowFuncMap windowFuncs = {
+static WindowFuncMap windowFuncs = {
   {
-    WindowType::Hann, [](int size){
+    WindowType::Hann,  [] ( int size){
       vector<double> result(size);
       for(int i = 0;i< size; i++){
         result[i] = 0.5 - 0.5 * std::cos((M_PI * 2 * i) / size );
@@ -26,7 +26,7 @@ WindowFuncMap windowFuncs = {
     }
   },
   {
-    WindowType::Hamming, [](int size){
+    WindowType::Hamming,  [] (int size){
       vector<double> result(size);
       for(int i = 0;i< size; i++){
         result[i] = 0.54  - 0.46 * std::cos((M_PI * 2 * i) / size );
