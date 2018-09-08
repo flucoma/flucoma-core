5906c4ddd3678014e5cb5c3bcf33cb11f790cc60
Owen Green <gungwho@gmail.com>
2018-09-08 18:45:25 +0100

    BaseSTFTClient: Add OLA gain compensation

--- include/clients/rt/BaseSTFTClient.hpp
+++ include/clients/rt/BaseSTFTClient.hpp
@@ -11,38 +11,65 @@
 
 namespace  fluid {
 namespace audio {
-    using fluid::stft::STFT;
-    using fluid::stft::ISTFT;
-    using fluid::parameter::STFTCheckParams;
+
 
     template <typename T, typename U>
-    class BaseSTFTClient:public STFTCheckParams, public BaseAudioClient<T,U>
+  class BaseSTFTClient:public BaseAudioClient<T,U>
     {
-        using data_type = fluid::FluidTensorView<T,2>;
+      using data_type = FluidTensorView<T,2>;
+      using vector    = FluidTensor<T,1>;
+      using complex   = FluidTensorView<std::complex<T>,1>;
+      
     public:
         BaseSTFTClient() = delete;
         BaseSTFTClient(BaseSTFTClient&) = delete;
         BaseSTFTClient operator=(BaseSTFTClient&) = delete;
 
         BaseSTFTClient(size_t windowsize, size_t hopsize, size_t fftsize):
-        STFTCheckParams(windowsize,hopsize,fftsize),
-        BaseAudioClient<T,U>(m_window_size,m_hop_size,1,2),
+        //stft::STFTCheckParams(windowsize,hopsize,fftsize),
+        BaseAudioClient<T,U>(windowsize,hopsize,1,1,2),
+        m_window_size(windowsize), m_hop_size(hopsize), m_fft_size(fftsize),
         m_stft(m_window_size, m_fft_size, m_hop_size),
         m_istft(m_window_size, m_fft_size, m_hop_size)
         {
+          //Make the product of the input and output windows
+          normWindow = m_stft.window();
+          normWindow.apply(m_istft.window(),[](double& x, double& y)
+          {
+            x *= y;
+          });
+//          std::cout << normWindow;
         }
 
         ~BaseSTFTClient() = default;
 
-        data_type process(data_type input)
+        void process(data_type input, data_type output) override
         {
-            return m_istft.processFrame(m_stft.processFrame(input.row(0)));
+            complex spec  = m_stft.processFrame(input.row(0));
+            output.row(0) = m_istft.processFrame(spec);
+            output.row(1) = normWindow;
+        }
+      
+        void post_process(data_type output) override
+        {
+          
+//          std::cout << "OUT\n" << output << '\n';
+          
+          output.row(0).apply(output.row(1),[](double& x, double g){
+              if(x)
+              {
+                x /= g ? g : 1;
+              }
+          });
         }
 
     private:
-        STFT m_stft;
-        ISTFT m_istft;
-
+      size_t m_window_size;
+      size_t m_hop_size;
+      size_t m_fft_size;
+      stft::STFT m_stft;
+      stft::ISTFT m_istft;
+      vector normWindow;
     };
 
 
