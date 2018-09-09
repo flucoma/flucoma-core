1b9b529da26e33837e7e86f9b3c0c34f363a72c5
Owen Green <gungwho@gmail.com>
2018-09-09 09:29:49 +0100

    BaseSTFTClient: Missing pragma once causing chaos in SC. Oh how I laughed

--- include/clients/rt/BaseSTFTClient.hpp
+++ include/clients/rt/BaseSTFTClient.hpp
@@ -4,10 +4,12 @@
  Base class for real-time STFT processes
 
 */
-
+#pragma once
 #include "clients/rt/BaseAudioClient.hpp"
-#include "clients/common/STFTCheckParams.hpp"
+//#include "clients/common/STFTCheckParams.hpp"
 #include "algorithms/STFT.hpp"
+
+#include <complex>
 
 namespace  fluid {
 namespace audio {
@@ -17,7 +19,7 @@ namespace audio {
   class BaseSTFTClient:public BaseAudioClient<T,U>
     {
       using data_type = FluidTensorView<T,2>;
-      using vector    = FluidTensor<T,1>;
+//      using vector    = FluidTensor<T,1>;
       using complex   = FluidTensorView<std::complex<T>,1>;
       
     public:
@@ -27,7 +29,7 @@ namespace audio {
 
         BaseSTFTClient(size_t windowsize, size_t hopsize, size_t fftsize):
         //stft::STFTCheckParams(windowsize,hopsize,fftsize),
-        BaseAudioClient<T,U>(windowsize,hopsize,1,1,2),
+        BaseAudioClient<T,U>(windowsize,hopsize,1,1,2 ),
         m_window_size(windowsize), m_hop_size(hopsize), m_fft_size(fftsize),
         m_stft(m_window_size, m_fft_size, m_hop_size),
         m_istft(m_window_size, m_fft_size, m_hop_size)
@@ -38,23 +40,19 @@ namespace audio {
           {
             x *= y;
           });
-//          std::cout << normWindow;
         }
 
-        ~BaseSTFTClient() = default;
-
+//        ~BaseSTFTClient() = default;
+//
         void process(data_type input, data_type output) override
         {
             complex spec  = m_stft.processFrame(input.row(0));
             output.row(0) = m_istft.processFrame(spec);
-            output.row(1) = normWindow;
+          output.row(1) = normWindow;
         }
-      
+//
         void post_process(data_type output) override
         {
-          
-//          std::cout << "OUT\n" << output << '\n';
-          
           output.row(0).apply(output.row(1),[](double& x, double g){
               if(x)
               {
@@ -69,7 +67,7 @@ namespace audio {
       size_t m_fft_size;
       stft::STFT m_stft;
       stft::ISTFT m_istft;
-      vector normWindow;
+      FluidTensor<T,1> normWindow;
     };
 
 
