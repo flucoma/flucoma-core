87e8ea0207d465cb77f4acd2f3027109b68104e6
Gerard <g.roma@hud.ac.uk>
2018-09-13 18:53:47 +0100

    Windows: add BlackmanHarris and Gaussian

--- include/algorithms/Windows.hpp
+++ include/algorithms/Windows.hpp
@@ -1,40 +1,58 @@
 #pragma once
 
 #include <cmath>
-#include <vector>
 #include <iostream>
 #include <map>
+#include <vector>
 
 namespace fluid {
 namespace windows {
 
-using std::vector;
-using std::map;
 using std::function;
+using std::map;
+using std::vector;
 
-
-enum class WindowType {Hann, Hamming};
+enum class WindowType { Hann, Hamming, BlackmanHarris, Gaussian };
 using WindowFuncMap = map<WindowType, function<vector<double>(int)>>;
 
 static WindowFuncMap windowFuncs = {
-  {
-    WindowType::Hann,  [] ( int size){
-      vector<double> result(size);
-      for(int i = 0;i< size; i++){
-        result[i] = 0.5 - 0.5 * std::cos((M_PI * 2 * i) / size );
-      }
-      return result;
-    }
-  },
-  {
-    WindowType::Hamming,  [] (int size){
-      vector<double> result(size);
-      for(int i = 0;i< size; i++){
-        result[i] = 0.54  - 0.46 * std::cos((M_PI * 2 * i) / size );
-      }
-      return result;
-    }
-  }
-};
-}
-}
+    {WindowType::Hann,
+     [](int size) {
+       vector<double> result(size);
+       for (int i = 0; i < size; i++) {
+         result[i] = 0.5 - 0.5 * std::cos((M_PI * 2 * i) / size);
+       }
+       return result;
+     }},
+    {WindowType::Hamming,
+     [](int size) {
+       vector<double> result(size);
+       for (int i = 0; i < size; i++) {
+         result[i] = 0.54 - 0.46 * std::cos((M_PI * 2 * i) / size);
+       }
+       return result;
+     }},
+    {WindowType::BlackmanHarris, [](int size) {
+       using std::cos;
+       vector<double> result(size);
+       for (int i = 0; i < size; i++) {
+         result[i] = 0.35875 - 0.48829 * cos((M_PI * 2 * i) / size) +
+                     0.14128 * cos((M_PI * 2 * i) / size) +
+                     0.01168 * cos((M_PI * 2 * i) / size);
+       }
+       return result;
+     }},
+     {WindowType::Gaussian, [](int size) {
+        using std::exp;
+        double sigma = size / 3;// TODO: should be argument
+        assert(size%2);
+        int h = (size - 1) / 2;
+        vector<double> result(size);
+        for (int i = -h; i <= h; i++) {
+          result[i+h] = exp(-i * i / (2 * sigma * sigma));
+        }
+        return result;
+      }}
+   };
+} // namespace windows
+} // namespace fluid
