306d97e4ab8c19a58cbfeb5c6eeaca850eeaf56a
Alex Harker <ajharker@gmail.com>
2018-09-14 14:06:43 +0100

    Onset segmentation is ready to be provided with input frames

--- include/algorithms/OnsetSegmentation.hpp
+++ include/algorithms/OnsetSegmentation.hpp
@@ -2,13 +2,23 @@
 #pragma once
 
 #include "Descriptors.hpp"
+#include "FFT.hpp"
+#include "Windows.hpp"
+#include <Eigen/Eigen>
+#include <algorithm>
 
 namespace fluid {
 namespace segmentation {
   
-class OnsetSegmentation
+using Eigen::Map;
+
+class OnsetSegmentation : public fft::FFT
 {
+  using RealTensor = fluid::FluidTensor<double, 1>;
   using Real = fluid::FluidTensorView<double, 1>;
+  using WindowType = windows::WindowType;
+  using ArrayXd = Eigen::ArrayXd;
+  using ArrayXcd = Eigen::ArrayXcd;
 
 public:
 
@@ -31,12 +41,66 @@ public:
     kModifiedKullbackLiebler
   };
   
-  OnsetSegmentation()
+  OnsetSegmentation(int FFTSize) : fft::FFT(FFTSize), mFFTSize(FFTSize), mWindowSize(0), mFrameDelta(0), mWindowType(windows::WindowType::Hann), mFunction(kL1Norm), mForwardOnly(false), mNormalisation(kNone)
   {
+    resizeStorage();
   }
 
+  void setParameters(DifferenceFunction function, bool forwardOnly, Normalisation normalisation)
+  {
+    mFunction = function;
+    mForwardOnly = forwardOnly;
+    mNormalisation = normalisation;
+  }
+  
+  void prepareStream(int windowSize, int frameDelta, WindowType windowType)
+  {
+    mWindowSize = std::min(mWindowSize, mFFTSize);
+    mFrameDelta = frameDelta;
+    mWindowType = windowType;
+    resizeStorage();
+  }
+  
+  int FFTSize() const         { return mFFTSize; }
+  int windowSize() const      { return mWindowSize; }
+  int frameDelta() const      { return mFrameDelta; }
+  int inputFrameSize() const  { return windowSize() + frameDelta(); }
+  
 private:
 
+  double processFrame(const double *input)
+  {
+    processSingleWindow(mFrame1, input + frameDelta());
+    processSingleWindow(mFrame2, input);
+    
+    Real frame1View(mFrame1);
+    Real frame2View(mFrame2);
+
+    return frameComparison(frame1View, frame2View);
+  }
+  
+  void processSingleWindow(RealTensor& frame, const double *input)
+  {
+    for (auto i = 0; i < frame.size(); i++)
+      mFFTBuffer(i) = input[i];
+    
+    mFFTBuffer *= mWindow;
+    Eigen::Ref<ArrayXcd> fftOut = FFT::process(Eigen::Ref<ArrayXd>(mFFTBuffer));
+    
+    for (auto i = 0; i < frame.size(); i++)
+    {
+      const double real = fftOut(i).real();
+      const double imag = fftOut(i).imag();
+      frame(i) = sqrt(real * real + imag * imag);
+    }
+  }
+  
+  void clipEpsilon(Real& input)
+  {
+    for (auto it = input.begin(); it != input.end(); it++)
+      *it = std::max(std::numeric_limits<double>::epsilon(), *it);
+  }
+  
   double frameComparison(Real& vec1, Real& vec2)
   {
     using namespace descriptors;
@@ -50,6 +114,11 @@ private:
       Descriptors::normalise(vec2, mNormalisation == kPower);
     }
     
+    // TODO - review this later
+    
+    clipEpsilon(vec1);
+    clipEpsilon(vec2);
+    
     switch (mFunction)
     {
       case kL1Norm:                      return Descriptors::differenceL1Norm(vec1, vec2);
@@ -62,12 +131,32 @@ private:
       case kModifiedKullbackLiebler:     return Descriptors::differenceMKL(vec1, vec2);
     }
   }
+  
+  void resizeStorage()
+  {
+    int FFTFrameSize = FFTSize() / 2 + 1;
+    mFrame1.resize(FFTFrameSize);
+    mFrame2.resize(FFTFrameSize);
+    mFFTBuffer.resize(windowSize());
     
+    mWindow = Map<ArrayXd>(windows::windowFuncs[mWindowType](windowSize()).data(), windowSize());
+  }
+  
 private:
 
-  Normalisation mNormalisation;
-  bool mForwardOnly;
+  RealTensor mFrame1;
+  RealTensor mFrame2;
+  ArrayXd mFFTBuffer;
+  ArrayXd mWindow;
+  
+  int mFFTSize;
+  int mWindowSize;
+  int mFrameDelta;
+  WindowType mWindowType;
+  
   DifferenceFunction mFunction;
+  bool mForwardOnly;
+  Normalisation mNormalisation;
 };
 
 };  // namespace segmentation
