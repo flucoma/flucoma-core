a97a40d27891c68c16a2e694f03cf40d3a532ea0
Alex Harker <ajharker@gmail.com>
2018-10-26 14:37:40 +0100

    Fixed typos and small code errors

--- include/clients/common/ParameterDescriptorList.hpp
+++ include/clients/common/ParameterDescriptorList.hpp
@@ -4,18 +4,22 @@
 
 #pragma once
 
-#include "clients/common/FluidParameters.hpp"
+#include "FluidParams.hpp"
 #include <vector>
 
 namespace fluid {
 namespace client {
-  class ParameterDescriptorList{
+    
+  using ParameterDescriptor = Descriptor;
+    
+  class ParameterDescriptorList
+  {
     using const_iterator = std::vector<ParameterDescriptor>::const_iterator;
     
   public:
-    const_iterator begin() { return mContainer.cbegin(); }
-    const_iterator end  () { return mContainer.cend(); }
-    size_t  size() const  {returbn mContainer.size(); }
+    const_iterator begin() const { return mContainer.cbegin(); }
+    const_iterator end() const { return mContainer.cend(); }
+    size_t size() const { return mContainer.size(); }
     
     const ParameterDescriptor& operator[](size_t index) const
     {
@@ -24,26 +28,11 @@ namespace client {
     
   protected:
     template<typename T>
-    addParameterDescriptor(T x) {}
-    
-  private:
-  
-//    struct Describable
-//    {
-//      virtual ~Describable() = default;
-//      virtual void set()
-//
-//    }
-//
-//    template <typename T>
-//    class ParameterDescriptor
-//    {
-//
-//    }
-//
+    void addParameterDescriptor(T x) { }
     
     std::vector<ParameterDescriptor> mContainer;
-  }
+  };
+    
 }
 }//namespace fluid
 
