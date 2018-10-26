93e3f03006ad4d11b2fc1701abf31d46071519f9
Owen Green <gungwho@gmail.com>
2018-10-26 14:46:46 +0100

    examples/CMakeLists.txt: Add hook for (empty) parmeterDescriporLIst test examples/test_paramDescriptorList.cpp: add

---
+++ examples/test_paramDescriptorList.cpp
@@ -0,0 +1,18 @@
+#include "clients/common/ParameterDescriptorList.hpp"
+#include "clients/common/ParameterInstance.hpp"
+
+
+void testPseudoEnvironment()
+{
+  
+  clients
+  
+  
+}
+
+
+
+int main(int argc, char *argv[]) {
+  
+  
+}
