2cf9bc63a881963b699452206417192b8ce0b9b7
Owen Green <gungwho@gmail.com>
2018-12-06 22:40:26 +0000

    Scrappy testing of STFT

--- examples/test_paramDescriptorList.cpp
+++ examples/test_paramDescriptorList.cpp
@@ -3,6 +3,8 @@
 #include <clients/rt/GainClient.hpp>
 #include <clients/rt/BaseSTFTClient.hpp>
 
+#include <random>
+
 //#include "clients/common/ParameterInstance.hpp"
 
 void testPseudoEnvironment() {
@@ -30,6 +32,12 @@ public:
     //    MaxClass_Base::makeClass<FluidMaxWrapper>(nameSpace,className);
     processParameters(params, std::index_sequence_for<Ts...>{});
     return DummyWrapper();
+  }
+
+
+  void process(std::vector<fluid::FluidTensorView<double,1>>& in,std::vector<fluid::FluidTensorView<double,1>>& out)
+  {
+    mClient.process(in,out);
   }
 
   template<size_t N,typename T>
@@ -74,9 +82,6 @@ private:
   template <typename T, size_t N> static void setterDispatch(Client *x) {
     impl::SetterDispatchImpl<Client, std::remove_const_t<T>, N>::f(x);
   }
-
-  
-
 };
 
 namespace impl {
@@ -114,9 +119,47 @@ int main(int argc, char *argv[]) {
 //  GainClient<double> g;
   DummyWrapper<BaseSTFTClient<double>, decltype(STFTParams)> c;
   //makeWrapper<BaseSTFTClient<double>>(STFTParams);
-  c.set<0>(1023); 
-  c.set<2>(8);
+  c.set<0>(1024);
+  c.set<1>(512);
+  c.set<2>(1024);
   
+  std::default_random_engine g;
+  std::uniform_real_distribution<double> noise(-1,1);
+  
+  fluid::FluidTensor<double,1> i(1024);
+  
+
+  
+  fluid::FluidTensor<double,1> o(1024);
+  std::vector<fluid::FluidTensorView<double,1>> input{i};
+  std::vector<fluid::FluidTensorView<double,1>> output{o};
+  
+  fluid::FluidTensor<double,1> collectInput(1024 * 256 + 1024);
+  fluid::FluidTensor<double,1> collectOutput((1024 * 256) + 1024);
+
+  
+  for(int j = 0; j < 256; j++)
+  {
+    i.apply([&g,&noise](double& x){
+      x = noise(g);
+    });
+    collectInput(fluid::Slice(1024 +  (1024*j),1024)) = i;
+    c.process(input,output);
+//    std::cout << *std::max_element(i.begin(), i.end()) << ' ' << *std::min_element(i.begin(), i.end()) << '\n';
+    collectOutput(fluid::Slice( 1024 * j,1024)) = o;
+//                              auto i = output[0];
+//      std::cout << *std::max_element(i.begin(), i.end()) << ' ' << *std::min_element(i.begin(), i.end()) << '\n';
+  }
+  std::cout << collectInput(fluid::Slice(1024,10));
+  std::cout << collectOutput(fluid::Slice(1024,10));
+     collectOutput.apply(collectInput,[](double& a, double& b){
+      a -= b;
+    });
+
+//    std::cout << *std::max_element(collectInput.begin(), collectInput.end()) << '\n';
+
+  std::cout << *std::max_element(collectOutput.begin() + 512, collectOutput.begin() + (1024 * 256)) << '\n';
+
   
 }
 
