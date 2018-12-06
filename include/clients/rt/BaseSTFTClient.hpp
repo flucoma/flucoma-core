8a6bb66d9bf9f86f442f3bf2cbe500207c2f5b9c
Owen Green <gungwho@gmail.com>
2018-12-06 22:39:58 +0000

    include/clients/rt/BaseSTFTClient.hpp: Reduce size by moving boiler plate include/clients/rt/BufferedProcess.hpp: Add STFTProcess to take STFT boiler plate to reusable place

--- include/clients/rt/BaseSTFTClient.hpp
+++ include/clients/rt/BaseSTFTClient.hpp
@@ -6,152 +6,61 @@ Test class for STFT pass-through
 #pragma once
 
 #include "BufferedProcess.hpp"
+#include <algorithms/public/STFT.hpp>
 #include <clients/common/FluidBaseClient.hpp>
-#include <clients/common/ParameterTypes.hpp>
 #include <clients/common/ParameterConstraints.hpp>
+#include <clients/common/ParameterTypes.hpp>
 #include <data/FluidTensor.hpp>
-#include <algorithms/STFT.hpp>
-//#include "clients/common/FluidParams.hpp"
-//#include "clients/common/STFTCheckParams.hpp"
-#include "clients/rt/BaseAudioClient.hpp"
+#include <data/TensorTypes.hpp>
 
 #include <complex>
 #include <string>
 #include <tuple>
-#include <vector> 
+#include <vector>
 
 namespace fluid {
 namespace client {
 
-enum STFTParamIndex {kWinsize,kHopsize,kFFTSize,kMaxWin};
+enum STFTParamIndex { kWinsize, kHopsize, kFFTSize, kMaxWin };
 
 auto constexpr STFTParams = std::make_tuple(
-  LongParam("winSize", "Window Size", 1024, Min(4)),
-  LongParam("hopSize", "Hop Size", 512),
-  LongParam("fftSize", "FFT Size", -1, LowerLimit<kWinsize>(),PowerOfTwo()),
-  LongParam("maxWinSize", "Maxiumm Window Size", 16384)
-);
-
+    LongParam("winSize", "Window Size", 1024, Min(4)),
+    LongParam("hopSize", "Hop Size", 512),
+    LongParam("fftSize", "FFT Size", -1, LowerLimit<kWinsize>(), PowerOfTwo()),
+    LongParam("maxWinSize", "Maxiumm Window Size", 16384));
 
 using Param_t = decltype(STFTParams);
 
 template <typename T, typename U = T>
 class BaseSTFTClient : public FluidBaseClient<Param_t> {
-  
+
   using View = FluidTensorView<T, 1>;
-  using ComplexView = FluidTensorView<std::complex<T>, 1>;
 
 public:
-//  static const std::vector<client::Descriptor> &getParamDescriptors() {
-//    static std::vector<client::Descriptor> params;
-//    if (params.size() == 0) {
-//      BaseAudioClient<T, U>::initParamDescriptors(params);
-//
-//      params.front().setDefault(1024);
-//      params[1].setDefault(512);
-//
-//      params.emplace_back("fftsize", "FFT Size", client::Type::kLong);
-//      params.back().setMin(-1).setInstantiation(true).setDefault(-1);
-//    }
-//
-//    return params;
-//  }
-
   BaseSTFTClient(BaseSTFTClient &) = delete;
   BaseSTFTClient operator=(BaseSTFTClient &) = delete;
 
-  BaseSTFTClient(): FluidBaseClient<Param_t>(STFTParams){
+  BaseSTFTClient() : FluidBaseClient<Param_t>(STFTParams) {
     audioChannelsIn(1);
     audioChannelsOut(1);
   }
 
-//  void reset() {
-//    size_t winsize = client::lookupParam("winsize", mParams).getLong();
-//    size_t hopsize = client::lookupParam("hopsize", mParams).getLong();
-//    size_t fftsize = client::lookupParam("fftsize", mParams).getLong();
-//
-//    mSTFT = std::unique_ptr<algorithm::STFT>(
-//        new algorithm::STFT(winsize, fftsize, hopsize));
-//    mISTFT = std::unique_ptr<algorithm::ISTFT>(
-//        new algorithm::ISTFT(winsize, fftsize, hopsize));
-//
-//    normWindow = mSTFT->window();
-//    normWindow.apply(mISTFT->window(), [](double &x, double &y) { x *= y; });
-//
-//    BaseAudioClient<T, U>::reset();
-//  }
+  void process(std::vector<RealVector> &input,
+               std::vector<RealVector> &output) {
 
-//  std::tuple<bool, std::string> sanityCheck() {
-//    //        BaseAudioClient<T,U>::getParams()[0].setLong(client::lookupParam("winsize",
-//    //        mParams).getLong());
-//    //        BaseAudioClient<T,U>::getParams()[1].setLong(client::lookupParam("hopsize",
-//    //        mParams).getLong());
-//
-//    for (auto &&p : getParams()) {
-//      std::tuple<bool, client::Instance::RangeErrorType> res = p.checkRange();
-//      if (!std::get<0>(res)) {
-//        switch (std::get<1>(res)) {
-//        case client::Instance::RangeErrorType::kMin:
-//          return {false, "Parameter below minimum"};
-//          break;
-//        case client::Instance::RangeErrorType::kMax:
-//          return {false, "Parameter above maximum"};
-//          break;
-//        }
-//      }
-//    }
-//
-//    std::tuple<bool, std::string> windowCheck =
-//        BaseAudioClient<T, U>::sanityCheck();
-//    if (!std::get<0>(windowCheck)) {
-//      return windowCheck;
-//    }
-//
-//    return client::checkFFTArguments(mParams[0], mParams[1], mParams[2]);
-//  }
-
-  // Here we do an STFT and its inverse
-  void process(std::vector<View>& input, std::vector<View> output) {
-    
-    mInputBuffer.push(input[0]);
-
-    mBufferedProcess.maxWindowSize(get<kMaxWin>()); 
-    mBufferedProcess.setBuffers(mInputBuffer, mOutputBuffer);
-    mBufferedProcess.hostSize(input.rows()); //safe?
-    mBufferedProcess.process(get<kWinsize>(), get(kHopsize),[this](View& in, View& out) {
-        auto spec = mSTFT->processFrame(in);
-        out = mISTFT->processFrame(spec);
-    } );
-    
-    
-
-//    output.row(1) = normWindow;
+    if (!input[0].data() || !output[0].data())
+      return;
+    // Here we do an STFT and its inverse
+    mSTFTBufferedProcess.process(
+        *this, input, output,
+        [](ComplexVector in, ComplexVector out) { out = in; });
   }
-  // Here we gain compensate for the OLA
-//  void postProcess(data_type output) override {
-//    output.row(0).apply(output.row(1), [](double &x, double g) {
-//      if (x) {
-//        x /= g ? g : 1;
-//      }
-//    });
-//  }
-//
-//  std::vector<client::Instance> &getParams() override { return mParams; }
 
-//private:
-//  void newParamSet() {
-//    mParams.clear();
-//    for (auto &&d : getParamDescriptors())
-//      mParams.emplace_back(d);
-//  }
-
-  std::unique_ptr<algorithm::STFT> mSTFT;
-  std::unique_ptr<algorithm::ISTFT> mISTFT;
-  FluidTensor<T, 1> normWindow;
-  FluidSource<double> mInputBuffer;
-  FluidSink<double> mOutputBuffer;
-  BufferedProcess mBufferedProcess;
-//  std::vector<client::Instance> mParams;
+private:
+  STFTBufferedProcess<double, BaseSTFTClient, kMaxWin, kWinsize, kHopsize,
+                      kFFTSize, true>
+      mSTFTBufferedProcess;
 };
 } // namespace client
 } // namespace fluid
+
--- include/clients/rt/BufferedProcess.hpp
+++ include/clients/rt/BufferedProcess.hpp
@@ -1,64 +1,169 @@
 #pragma once
 
 #include <data/FluidTensor.hpp>
-#include <data/FluidBuffers.hpp>
+#include <data/TensorTypes.hpp>
 
-namespace fluid{
-namespace client{
-class BufferedProcess
-{
+#include <algorithms/public/STFT.hpp>
+#include <clients/common/FluidSink.hpp>
+#include <clients/common/FluidSource.hpp>
+
+namespace fluid {
+namespace client {
+
+class BufferedProcess {
 public:
-  template<typename F>
-  void process(std::size_t windowSize, std::size_t hopSize, F processFunc){
-    
-      assert(windowSize <= maxWindowSize() && "Window bigger than maximum");
-    
-      for( ;mFrameTime< mHostSize; mFrameTime += hopSize)
-      {
-        if(mSource)
-        {
-          FluidTensorView<double, 2> windowIn = mFrameIn(Slice(0,windowSize),Slice(0));
-          FluidTensorView<double, 2> windowOut = mFrameOut(Slice(0,windowSize),Slice(0));
+  template <typename F>
+  void process(std::size_t windowSize, std::size_t hopSize, F processFunc) {
 
+    assert(windowSize <= maxWindowSize() && "Window bigger than maximum");
 
-          mSource->pull(windowIn, mFrameTime);
-          processFunc(windowIn, windowOut);
-          if (mSink)
-            mSink->push(windowOut, mFrameTime);
-        }
+    for (; mFrameTime < mHostSize; mFrameTime += hopSize) {
+      if (mSource) {
+        RealMatrix windowIn = mFrameIn(Slice(0), Slice(0, windowSize));
+        FluidTensorView<double, 2> windowOut =
+            mFrameOut(Slice(0), Slice(0, windowSize));
+
+        mSource->pull(windowIn, mFrameTime);
+        processFunc(windowIn, windowOut);
+
+        //      std::cout << mFrameTime << ' ' << hopSize <<'\n';
+
+        if (mSink)
+          mSink->push(windowOut, mFrameTime);
       }
-    
-      mFrameTime = mFrameTime < mHostSize ? mFrameTime
-                                                : mFrameTime - mHostSize;
+    }
 
-    
+    mFrameTime = mFrameTime < mHostSize ? mFrameTime : mFrameTime - mHostSize;
   }
-  
+
   std::size_t hostSize() const noexcept { return mHostSize; }
   void hostSize(std::size_t size) noexcept { mHostSize = size; }
-  
-  std::size_t maxWindowSize() const noexcept { return mFrameIn.rows(); }
-  void maxWindowSize(std::size_t size) {
-    if(size > mFrameIn.rows())
-      mFrameIn.resize(size);
-    if(size > mFrameOut.rows())
-      mFrameOut.resize(size);
 
+  std::size_t maxWindowSize() const noexcept { return mFrameIn.cols(); }
+
+  void maxSize(std::size_t frames, std::size_t channelsIn,
+               std::size_t channelsOut) {
+    if (channelsIn > mFrameIn.rows() || frames > mFrameIn.cols())
+      mFrameIn.resize(channelsIn, frames);
+    if (channelsOut > mFrameOut.rows() || frames > mFrameOut.cols())
+      mFrameOut.resize(channelsOut, frames);
   }
-  
-  void setBuffers(FluidSource<double>& source,FluidSink<double>& sink)
-  {
+
+  void setBuffers(FluidSource<double> &source, FluidSink<double> &sink) {
     mSource = &source;
-    mSink = &sink; 
+    mSink = &sink;
   }
-  
+
 private:
   std::size_t mFrameTime;
   std::size_t mHostSize;
-  FluidTensor<double,2> mFrameIn;
-  FluidTensor<double,2> mFrameOut;
-  FluidSource<double>* mSource = nullptr;
-  FluidSink<double>* mSink = nullptr;
+  FluidTensor<double, 2> mFrameIn;
+  FluidTensor<double, 2> mFrameOut;
+  FluidSource<double> *mSource = nullptr;
+  FluidSink<double> *mSink = nullptr;
 };
-} //namespace client
-} //namespace fluid
+
+template <typename T, typename Client, size_t maxWinParam, size_t winParam,
+          size_t hopParam, size_t FFTParam, bool normalise>
+class STFTBufferedProcess {
+  using View = FluidTensorView<T, 1>;
+
+public:
+  template <typename F>
+  void process(Client &x, std::vector<View> &input, std::vector<View> &output,
+               F &&processFunc) {
+
+    size_t winSize = x.template get<winParam>();
+    size_t hopSize = x.template changed<hopParam>() ? x.template get<hopParam>()
+                                                    : winSize / 2;
+    size_t fftSize =
+        x.template changed<FFTParam>() ? x.template get<FFTParam>() : winSize;
+
+    // TODO: constraints check here: error and bail if unmet
+
+    bool newParams = paramsChanged(winSize, hopSize, fftSize);
+
+    if (!mSTFT.get() || newParams)
+      mSTFT.reset(new algorithm::STFT(winSize, fftSize, hopSize));
+
+    if (!mISTFT.get() || newParams)
+      mISTFT.reset(new algorithm::ISTFT(winSize, fftSize, hopSize));
+
+    if (!input[0].data())
+      return; // if there's not actually an audio input, no point continuing
+
+    std::size_t chansIn = x.audioChannelsIn();
+    std::size_t chansOut = x.audioChannelsOut();
+
+    size_t hostBufferSize = input[0].size();
+    mBufferedProcess.hostSize(hostBufferSize); // safe assumption?
+    mInputBuffer.setHostBufferSize(hostBufferSize);
+    mOutputBuffer.setHostBufferSize(hostBufferSize);
+
+    std::size_t maxWin = x.template get<maxWinParam>();
+    mInputBuffer.setSize(maxWin);
+    mOutputBuffer.setSize(maxWin);
+    mInputBuffer.reset(chansIn);
+    // TODO: make explicit the extra channel for post-normalisation
+    mOutputBuffer.reset(chansOut + 1);
+    mBufferedProcess.maxSize(maxWin, chansIn, chansOut + 1);
+
+    if (maxWin > mFrameAndWindow.cols())
+      mFrameAndWindow.resize(chansOut + 1, maxWin);
+    if ((fftSize / 2 + 1) != mSpectrogram.cols())
+      mSpectrogram.resize(2, (fftSize / 2 + 1));
+
+    mBufferedProcess.setBuffers(mInputBuffer, mOutputBuffer);
+
+    mInputBuffer.push(RealMatrix(input[0]));
+
+    mBufferedProcess.process(
+        winSize, hopSize,
+        [this, &processFunc](RealMatrix &in, RealMatrix &out) {
+          mSTFT->processFrame(in.row(0), mSpectrogram.row(0));
+          processFunc(mSpectrogram.row(0), mSpectrogram.row(1));
+          mISTFT->processFrame(mSpectrogram.row(1), out.row(0));
+          out.row(1) = mSTFT->window();
+          out.row(1).apply(mISTFT->window(),
+                           [](double &x, double &y) { x *= y; });
+        });
+
+    // TODO: if normalise
+    RealMatrix unnormalisedFrame =
+        mFrameAndWindow(Slice(0), Slice(0, hostBufferSize));
+    mOutputBuffer.pull(unnormalisedFrame);
+    unnormalisedFrame.row(0).apply(unnormalisedFrame.row(1),
+                                   [](double &x, double g) {
+                                     if (x) {
+                                       x /= g ? g : 1;
+                                     }
+                                   });
+    if (output[0].data())
+      output[0] = unnormalisedFrame.row(0);
+  }
+
+private:
+  bool paramsChanged(std::size_t winSize, std::size_t hopSize,
+                     std::size_t fftSize) {
+    static std::size_t win, hop, fft;
+    bool res = (win == winSize) && (hop == hopSize) && (fft == fftSize);
+
+    win = winSize;
+    hop = hopSize;
+    fft = fftSize;
+
+    return res;
+  }
+
+  FluidTensor<double, 2> mFrameAndWindow;
+  FluidTensor<std::complex<double>, 2> mSpectrogram;
+  std::unique_ptr<algorithm::STFT> mSTFT;
+  std::unique_ptr<algorithm::ISTFT> mISTFT;
+  BufferedProcess mBufferedProcess;
+  FluidSource<double> mInputBuffer;
+  FluidSink<double> mOutputBuffer;
+};
+
+} // namespace client
+} // namespace fluid
+
