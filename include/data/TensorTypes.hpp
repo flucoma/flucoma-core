77604449aeea27dd4dd76f2a2623d8d5b8126bd3
Owen Green <gungwho@gmail.com>
2018-12-12 13:06:39 +0000

    include/data/TensorTypes.hpp: Merge changes to aliases from refactor tip

--- include/data/TensorTypes.hpp
+++ include/data/TensorTypes.hpp
@@ -2,27 +2,18 @@
 
 #include "FluidTensor.hpp"
 #include <complex>
-#include <type_traits>
 
 namespace fluid {
   using std::complex;
 
-  using RealMatrix = FluidTensorView<double, 2>;
-  using ComplexMatrix = FluidTensorView<complex<double>, 2>;
-  using RealVector = FluidTensorView<double, 1>;
-  using ComplexVector = FluidTensorView<complex<double>, 1>;
-  using ComplexMatrix = FluidTensorView<complex<double>, 2>;
+  using RealMatrix = FluidTensor<double, 2>;
+  using ComplexMatrix = FluidTensor<complex<double>, 2>;
+  using RealVector = FluidTensor<double, 1>;
+  using ComplexVector = FluidTensor<complex<double>, 1>;
 
+  using RealMatrixView = FluidTensorView<double, 2>;
+  using ComplexMatrixView = FluidTensorView<complex<double>, 2>;
+  using RealVectorView = FluidTensorView<double, 1>;
+  using ComplexVectorView = FluidTensorView<complex<double>, 1>;
 
-  template<typename T>
-  using HostVector = FluidTensorView<T,1>;
-
-  template<typename T>
-  using HostMatrix = FluidTensorView<T,2>;
-
-  template<class Matrix, typename T=typename Matrix::type, size_t N=Matrix::order>
-  using IsView = std::is_same< Matrix, FluidTensorView<T,N>>;
-  
-  template<class Matrix, class = std::enable_if_t<IsView<Matrix>::value>>
-  using Data = FluidTensor<typename Matrix::type, Matrix::order>;
 } // namespace fluid
