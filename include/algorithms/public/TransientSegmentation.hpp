ac1946edeb024599f431e44e9279494d55576f46
Gerard <g.roma@hud.ac.uk>
2019-01-31 11:25:53 +0100

    porting debounce updates to transient segmentaiton from master

--- include/algorithms/public/TransientSegmentation.hpp
+++ include/algorithms/public/TransientSegmentation.hpp
@@ -11,17 +11,19 @@ class TransientSegmentation : private algorithm::TransientExtraction {
 public:
   TransientSegmentation(size_t order, size_t iterations, double robustFactor)
       : TransientExtraction(order, iterations, robustFactor, false),
-        mHoldTime(25), mDebounce(0) {}
+        mMinSegment(25), mDebounce(0), mLastDetection(false) {}
 
   void setDetectionParameters(double power, double threshHi, double threshLo,
-                              int halfWindow = 7, int hold = 25) {
+                              int halfWindow = 7, int hold = 25,
+                              int minSegment = 50) {
     TransientExtraction::setDetectionParameters(power, threshHi, threshLo,
                                                 halfWindow, hold);
-    mHoldTime = hold;
+    mMinSegment = minSegment;
   }
 
   void prepareStream(int blockSize, int padSize) {
     TransientExtraction::prepareStream(blockSize, padSize);
+    mLastDetection = false;
     mDebounce = 0;
   }
 
@@ -36,17 +38,19 @@ public:
     detect(input.data(), input.extent(0));
     const double *transientDetection = getDetect();
     for (int i = 0; i < std::min<size_t>(hopSize(), output.size()); i++) {
-      output(i) = (transientDetection[i] && !mDebounce);
-      mDebounce = transientDetection[i] ? mHoldTime : std::max(0, --mDebounce);
+      output(i) = (transientDetection[i] && !mLastDetection && !mDebounce);
+      mDebounce = output(i) == 1.0 ? mMinSegment : std::max(0, --mDebounce);
+      mLastDetection = transientDetection[i] == 1.0;
     }
   }
 
 private:
-  //void resizeStorage() { mDetect.resize(hopSize(), 0.0); }
+  // void resizeStorage() { mDetect.resize(hopSize(), 0.0); }
 
 private:
-  int mHoldTime;
+  int mMinSegment;
   int mDebounce;
+  bool mLastDetection;
   // std::vector<double> mDetect;
 };
 
