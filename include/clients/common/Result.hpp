9bae2c41c3e88a08412526d5214dc15aea0a78af
Owen Green <gungwho@gmail.com>
2019-03-03 23:04:13 +0000

    Improve Result interface, start message list class include/clients/common/Result.hpp

--- include/clients/common/Result.hpp
+++ include/clients/common/Result.hpp
@@ -7,14 +7,19 @@ class Result {
 public:
   enum class Status {kOk, kWarning, kError};
   
-  Result(Status status, std::string msg):mStatus(status),mMsg(msg){}
+  Result(Status s, std::string msg):mStatus(s),mMsg(msg){}
 
-  template <typename...Ts>
-  Result(Status status, Ts...msgChunks):mStatus(status)
+  template<typename...Args>
+  Result(Status s,Args...args):mStatus(s)
   {
-   std::initializer_list<int>{(addMessage(msgChunks),0)...};
+    std::initializer_list<int>{(mMsg<<args,0)...};
   }
 
+//  template <typename...Ts>
+//  Result(Status status, Ts...msgChunks):mStatus(status)
+//  {
+//   std::initializer_list<int>{(addMessage(msgChunks),0)...};
+//  }
 
   Result() = default;
   Result(Result& x):mStatus(x.mStatus),mMsg(x.mMsg.str())
@@ -35,16 +40,19 @@ public:
   Status status() { return mStatus; }
   
   void set(Status r) noexcept { mStatus = r; }
+  
   std::string message() const noexcept { return mMsg.str(); }
-  template<typename T>
-  void addMessage(T str)
+  
+  template<typename...Ts>
+  void addMessage(Ts...args)
   {
-    mMsg << str;
+    std::initializer_list<int>{(mMsg<<args,0)...};
   }
   
   void reset()
   {
-    mMsg.clear();
+    std::stringstream newMsg;
+    std::swap(mMsg,newMsg); 
     mStatus = Status::kOk;
   }
   
@@ -53,5 +61,44 @@ private:
   std::stringstream mMsg;
 };
 
+
+class MessageList
+{
+public:
+  
+  template<typename...Args>
+  void warn(Args&&...args)
+  {
+    add<Result::Status::kWarning>(std::forward<Args>(args)...);
+  }
+  
+  template<typename...Args>
+  void error(Args&&...args)
+  {
+    add<Result::Status::kError>(std::forward<Args>(args)...);
+  }
+
+  template<typename...Args>
+  void info(Args&&...args)
+  {
+    add<Result::Status::kOk>(std::forward<Args>(args)...);
+  }
+  
+  auto release()
+  {
+    auto res = std::vector<Result>{};
+    std::swap(messages,res);
+    return res;
+  }
+private:
+
+  template<Result::Status S, typename...Args>
+  void add(Args&&...args)
+  {
+    messages.emplace_back(S,std::forward<Args>(args)...);
+  }
+
+  std::vector<Result> messages;
+};
 }
 }
