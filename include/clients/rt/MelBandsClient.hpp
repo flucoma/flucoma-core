77e81b040047a0aed8731dc254c7abab3fa7a327
Gerard <g.roma@hud.ac.uk>
2019-05-13 12:50:52 +0200

    split mfcc into melbands and mfcc

--- include/clients/rt/MelBandsClient.hpp
+++ include/clients/rt/MelBandsClient.hpp
@@ -19,43 +19,39 @@ using algorithm::DCT;
 using algorithm::MelBands;
 
 enum MFCCParamIndex {
-  kNCoefs,
   kNBands,
   kMinFreq,
   kMaxFreq,
-  kMaxNCoefs,
+  kMaxNBands,
   kFFT,
   kMaxFFTSize
 };
 
-auto constexpr MFCCParams = defineParameters(
-
-    LongParam("numCoefs", "Number of Cepstral Coefficients", 13, Min(2),
-              UpperLimit<kNBands>()),
+auto constexpr MelBandsParams = defineParameters(
     LongParam("numBands", "Number of Bands", 40, Min(2),
-              FrameSizeUpperLimit<kFFT>()),
+              UpperLimit<kMaxNBands>()),
     FloatParam("minFreq", "Low Frequency Bound", 20, Min(0)),
     FloatParam("maxFreq", "High Frequency Bound", 20000, Min(0)),
-    LongParam<Fixed<true>>("maxNumCoefs", "Maximum Number of Coefficients", 40,
-                           MaxFrameSizeUpperLimit<kMaxFFTSize>(), Min(2)),
+    LongParam<Fixed<true>>("maxNumBands", "Maximum Number of Bands", 120,
+                           Min(2), MaxFrameSizeUpperLimit<kMaxFFTSize>()),
     FFTParam<kMaxFFTSize>("fftSettings", "FFT Settings", 1024, -1, -1),
     LongParam<Fixed<true>>("maxFFTSize", "Maxiumm FFT Size", 16384));
 
 template <typename T>
-class MFCCClient : public FluidBaseClient<decltype(MFCCParams), MFCCParams>,
-                   public AudioIn,
-                   public ControlOut
+class MelBandsClient
+    : public FluidBaseClient<decltype(MelBandsParams), MelBandsParams>,
+      public AudioIn,
+      public ControlOut
 
 {
   using HostVector = HostVector<T>;
 
 public:
-  MFCCClient(ParamSetViewType &p)
+  MelBandsClient(ParamSetViewType &p)
       : FluidBaseClient{p}, mSTFTBufferedProcess(get<kMaxFFTSize>(), 1, 0) {
     mBands = FluidTensor<double, 1>(get<kNBands>());
-    mCoefficients = FluidTensor<double, 1>(get<kNCoefs>());
     FluidBaseClient::audioChannelsIn(1);
-    FluidBaseClient::controlChannelsOut(get<kMaxNCoefs>());
+    FluidBaseClient::controlChannelsOut(get<kMaxNBands>());
   }
 
   void process(std::vector<HostVector> &input,
@@ -68,24 +64,21 @@ public:
     assert(output.size() >= FluidBaseClient::controlChannelsOut() &&
            "Too few output channels");
 
-    if (mTracker.changed(get<kFFT>().frameSize(), get<kNCoefs>(),
-                         get<kNBands>(), get<kMinFreq>(), get<kMaxFreq>())) {
+    if (mTracker.changed(get<kFFT>().frameSize(), get<kNBands>(),
+                         get<kMinFreq>(), get<kMaxFreq>())) {
       mMagnitude.resize(get<kFFT>().frameSize());
       mBands.resize(get<kNBands>());
-      mCoefficients.resize(get<kNCoefs>());
       mMelBands.init(get<kMinFreq>(), get<kMaxFreq>(), get<kNBands>(),
                      get<kFFT>().frameSize(), sampleRate());
-      mDCT.init(get<kNBands>(), get<kNCoefs>());
     }
 
     mSTFTBufferedProcess.processInput(
         mParams, input, [&](ComplexMatrixView in) {
           algorithm::STFT::magnitude(in.row(0), mMagnitude);
           mMelBands.processFrame(mMagnitude, mBands);
-          mDCT.processFrame(mBands, mCoefficients);
         });
-    for (int i = 0; i < get<kNCoefs>(); ++i)
-      output[i](0) = mCoefficients(i);
+    for (int i = 0; i < get<kNBands>(); ++i)
+      output[i](0) = mBands(i);
   }
 
   size_t latency() { return get<kFFT>().winSize(); }
@@ -93,21 +86,20 @@ public:
   size_t controlRate() { return get<kFFT>().hopSize(); }
 
 private:
-  ParameterTrackChanges<size_t, size_t, size_t, double, double> mTracker;
+  ParameterTrackChanges<size_t, size_t, double, double> mTracker;
   STFTBufferedProcess<ParamSetViewType, T, kFFT, false> mSTFTBufferedProcess;
   MelBands mMelBands;
-  DCT mDCT;
   FluidTensor<double, 1> mMagnitude;
   FluidTensor<double, 1> mBands;
-  FluidTensor<double, 1> mCoefficients;
 };
 
-auto constexpr NRTMFCCParams =
-    makeNRTParams<MFCCClient>({BufferParam("source", "Source Buffer")},
-                              {BufferParam("features", "Output Buffer")});
-template <typename T>
-using NRTMFCCClient = NRTControlAdaptor<MFCCClient<T>, decltype(NRTMFCCParams),
-                                        NRTMFCCParams, 1, 1>;
+auto constexpr NRTMelBandsParams =
+    makeNRTParams<MelBandsClient>({BufferParam("source", "Source Buffer")},
+                                  {BufferParam("features", "Output Buffer")});
 
+template <typename T>
+using NRTMelBandsClient =
+    NRTControlAdaptor<MelBandsClient<T>, decltype(NRTMelBandsParams),
+                      NRTMelBandsParams, 1, 1>;
 } // namespace client
 } // namespace fluid
