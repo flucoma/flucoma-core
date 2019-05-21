ebb1618c9d4c9912d39130ea348d04e40e8ef2fe
Alex Harker <ajharker@gmail.com>
2019-05-21 10:22:03 +0100

    Removed rank from the buffer adaptor and put calculations into the NMF clients for rank/channels->buffer channel

--- include/clients/nrt/FluidNRTClientWrapper.hpp
+++ include/clients/nrt/FluidNRTClientWrapper.hpp
@@ -249,7 +249,7 @@ struct Streaming
     {
       if(!outputBuffers[i]) continue;
       BufferAdaptor::Access thisOutput(outputBuffers[i]);
-      thisOutput.resize(nFrames,nChans,1,sampleRate);
+      thisOutput.resize(nFrames,nChans,sampleRate);
       for(int j = 0; j < nChans; ++j)
         thisOutput.samps(j) = outputData[i].row(j)(Slice(padding));
     }
@@ -305,7 +305,7 @@ struct StreamingControl
     }
     
     BufferAdaptor::Access thisOutput(outputBuffers[0]);
-    thisOutput.resize(nHops - 1,nChans * nFeatures,1,sampleRate / controlRate);
+    thisOutput.resize(nHops - 1,nChans * nFeatures,sampleRate / controlRate);
 
     for(int i = 0; i < nFeatures; ++i)
     {
