5a62ab74a152cf1097cdc682ed2c439ab3bbf6db
Alex Harker <ajharker@gmail.com>
2019-05-23 15:21:12 +0100

    Added fluid task

---
+++ include/util/FluidTask.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <atomic>
+
+namespace fluid {
+
+class FluidTask
+{
+public:
+  
+  FluidTask() : mProgress(0.0), mCancel(false)
+  {}
+    
+  void processUpdate(double samplesDone, double taskLength)
+  {
+    bool cancel = mCancel;
+    mProgress = samplesDone / taskLength;
+    mCancel = false;
+  }
+    
+  void reset()
+  {
+    mCancel = false;
+  }
+    
+  double progress() { return mProgress; }
+    
+private:
+  std::atomic<double> mProgress;
+  bool mCancel;
+};
+
+} // namespace fluid
