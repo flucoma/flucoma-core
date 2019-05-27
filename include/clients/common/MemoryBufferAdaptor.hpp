ce70ace78c06482fcd0b2646217792826625a3ec
Owen Green <gungwho@gmail.com>
2019-05-28 00:39:04 +0100

    De-rankify include/clients/common/MemoryBufferAdaptor.hpp

--- include/clients/common/MemoryBufferAdaptor.hpp
+++ include/clients/common/MemoryBufferAdaptor.hpp
@@ -23,10 +23,9 @@ public:
       if(this != other.get())
       {
         BufferAdaptor::Access src(other.get());
-        mData.resize(src.numFrames(),src.numChans() * src.rank());
+        mData.resize(src.numFrames(),src.numChans());
         mExists = src.exists();
         mValid = src.valid();
-        mRank = src.rank();
         mSampleRate = src.sampleRate();
         for(size_t i = 0; i < mData.cols(); i++)
           mData.col(i) = src.samps(0, src.numFrames(), i);
@@ -43,13 +42,12 @@ public:
       BufferAdaptor::Access src(mOrigin.get());
       if(src.exists())
       {
-        if(numChans() != src.numChans() || numFrames() != src.numFrames() || mRank != src.rank())
-          src.resize(numFrames(),numChans(),mRank,mSampleRate);
+        if(numChans() != src.numChans() || numFrames() != src.numFrames())
+          src.resize(numFrames(),numChans(),mSampleRate);
         
         if(src.valid())
           for(int i = 0; i < numChans(); ++i)
-            for(int j = 0; j < mRank; ++j)
-              src.samps(i, j) = samps(i, j);
+              src.samps(i) = samps(i);
       }
       //TODO feedback failure to user somehow: I need a message queue
       
@@ -61,20 +59,18 @@ public:
    bool valid() const override { return mValid; }
    bool exists() const override  { return mExists; }
     
-   void resize(size_t frames, size_t channels, size_t rank, double sampleRate) override
+   void resize(size_t frames, size_t channels, double sampleRate) override
    {
      mWrite = true;
-     mRank = rank;
      mSampleRate = sampleRate;
-     mData.resize(frames,channels * rank);
+     mData.resize(frames,channels);
    }
     
    // Return a slice of the buffer
-   FluidTensorView<float, 1> samps(size_t channel, size_t rankIdx = 0) { return mData.col(channel * mRank + rankIdx); }
+   FluidTensorView<float, 1> samps(size_t channel) { return mData.col(channel); }
    FluidTensorView<float, 1> samps(size_t offset, size_t nframes, size_t chanoffset) { return mData(Slice(offset, nframes), Slice(chanoffset, 1)).col(0); }
    size_t numFrames() const override { return mData.rows(); }
-   size_t numChans() const override { return mRank ? (mData.cols() / mRank) : 0; }
-   size_t rank() const override { return mRank; }
+   size_t numChans() const override { return mData.cols(); }
    double sampleRate() const { return mSampleRate; }
   
   private:
@@ -84,7 +80,6 @@ public:
     bool mValid{true};
     bool mExists{true};
     bool mWrite{false};
-    int mRank{1};
 };
     
 }
