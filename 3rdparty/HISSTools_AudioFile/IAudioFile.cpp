3fae157fb0d6cd34f64452b587d7e8bc08394f2a
Owen Green <gungwho@gmail.com>
2019-07-04 23:49:55 +0100

    Missing <cstring> in I and O AudioFile.hpp

--- 3rdparty/HISSTools_AudioFile/IAudioFile.cpp
+++ 3rdparty/HISSTools_AudioFile/IAudioFile.cpp
@@ -2,6 +2,7 @@
 
 #include <cmath>
 #include <cassert>
+#include <cstring>
 #include <vector>
 #include <algorithm>
 
--- 3rdparty/HISSTools_AudioFile/OAudioFile.cpp
+++ 3rdparty/HISSTools_AudioFile/OAudioFile.cpp
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <cmath>
+#include <cstring>
 #include <vector>
 
 namespace HISSTools
