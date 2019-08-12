47c0baef84dc0eae9df6237ae005ee6b49d831fe
Owen Green <gungwho@gmail.com>
2019-08-12 18:34:44 +0100

    include/clients/nrt/FluidNRTClientWrapper.hpp: Return from functions that expect it, for happy stacks

--- include/clients/nrt/FluidNRTClientWrapper.hpp
+++ include/clients/nrt/FluidNRTClientWrapper.hpp
@@ -351,7 +351,7 @@ struct Slicing
 
     client.process(input,output,true);
 
-    impl::spikesToTimes(onsetPoints(0,Slice(padding,nFrames)), outputBuffers[0], 1, inputBuffers[0].startFrame, nFrames,src.sampleRate());
+    return impl::spikesToTimes(onsetPoints(0,Slice(padding,nFrames)), outputBuffers[0], 1, inputBuffers[0].startFrame, nFrames,src.sampleRate());
   }
 };
 
