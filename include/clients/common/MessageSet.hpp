7f87e456a9b3605b532caa544e52c50e7907694e
Owen Green <gungwho@gmail.com>
2019-08-12 18:28:04 +0100

    MessageSet to forward lambdas, as an alternative to trying to store pmfs

--- include/clients/common/MessageSet.hpp
+++ include/clients/common/MessageSet.hpp
@@ -8,24 +8,36 @@
 namespace fluid {
 namespace client {
 
-template<typename Ret, typename T, typename...Args>
+template<typename L, typename Ret, typename T, typename...Args>
 struct Message
 {
-  constexpr Message(const char* n, Ret (T::* pmf) (Args...)): name{n}, op(pmf) {}
+  Message(const char* n, L l): name{n}, op(l) {}
   const char* name;
-  Ret (T::* op) (Args...);
+  L op;
   using ReturnType = Ret;
   using IndexList = std::index_sequence_for<Args...>;
   using ArgumentTypes = std::tuple<Args...>;
   using Client = T;
   decltype(auto) operator()(Client& c, Args...args) const
   {
-    return (c.*op)(std::forward<Args>(args)...);
+    return op(c,args...); 
+    //(c.*op)(std::forward<Args>(args)...);
   }
 };
 
 template<typename Ret, typename T, typename...Args>
-constexpr auto makeMessage(const char* name, Ret (T::* pmf) (Args...)) { return Message<Ret,T,Args...>{name, pmf}; }
+auto makeMessage(const char* name, Ret (T::* pmf) (Args...)  )
+{
+  auto l  = [=](T& t, Args...args){ return (t.*pmf)(args...);};
+  return Message<std::decay_t<decltype(l)>,Ret,T,Args...>{name, l};
+}
+
+template<typename Ret, typename T, typename...Args>
+auto makeMessage(const char* name, Ret (T::* pmf) (Args...) const )
+{
+  auto l  = [=](T& t, Args...args){ return (t.*pmf)(args...);};
+  return Message<std::decay_t<decltype(l)>,Ret,T,Args...>{name, l};
+}
 
 template <typename>
 class MessageSet;
@@ -43,13 +55,13 @@ public:
   
   using IndexList = std::index_sequence_for<Ts...>;
   
-  constexpr MessageSet(const Ts&&... ts) : mMessages{std::make_tuple(ts...)} {}
-  constexpr MessageSet(const std::tuple<Ts...>&& t): mMessages{t} {}
+  MessageSet(const Ts&&... ts) : mMessages{std::make_tuple(ts...)} {}
+  MessageSet(const std::tuple<Ts...>&& t): mMessages{t} {}
 
   template<size_t N>
   std::string name() const { return std::get<N>(mMessages).name; }
 
-  constexpr size_t size() const noexcept { return sizeof...(Ts); }
+  size_t size() const noexcept { return sizeof...(Ts); }
   
   template <template <size_t N, typename T> class Func>
   void iterate() const
@@ -58,7 +70,7 @@ public:
   }
   
   template <size_t N>
-  constexpr auto& get() const
+  auto& get() const
   {
     return std::get<0>(std::get<N>(mMessages));
   }
@@ -79,14 +91,14 @@ private:
 };
 
 template<typename...Args>
-constexpr MessageSet<std::tuple<Args...>> defineMessages(Args&&...args){ return {std::forward<Args>(args)...}; }
+MessageSet<std::tuple<Args...>> defineMessages(Args&&...args){ return {std::forward<Args>(args)...}; }
 
 //Boilerplate macro for clients
 #define FLUID_DECLARE_MESSAGES(...) \
     using MessageType = std::add_const_t<decltype(defineMessages(__VA_ARGS__))>; \
-    static constexpr MessageType getMessageDescriptors() { return defineMessages(__VA_ARGS__);}
+    static MessageType getMessageDescriptors() { return defineMessages(__VA_ARGS__);}
 
-auto constexpr NoMessages = defineMessages();
+auto NoMessages = defineMessages();
 
 } //client
 } //fluid
