d6783f909ad7d93b0e4b072a048e9e08f8c0c046
Owen Green <gungwho@gmail.com>
2019-08-20 11:46:24 +0100

    Thread adaptor: Split enqueuing a job and processing into two functions, so that we can get unsurprsing behaviour in max when a chain of commands is triggered from the scheduler, and we are in synchronous mode (i.e. we can enqueue with the correct params from the scheduler, then run derferred)

--- include/clients/nrt/FluidNRTClientWrapper.hpp
+++ include/clients/nrt/FluidNRTClientWrapper.hpp
@@ -422,19 +422,31 @@ public:
     }
   }
   
+  Result enqueue(ParamSetType& p)
+  {
+    if (mThreadedTask && (mSynchronous|| !mQueueEnabled) )
+      return {Result::Status::kError, "already processing"};
+
+    mQueue.push_back(p);
+    
+    return {};
+  }
+  
   Result process()
   {
     if (mThreadedTask && (mSynchronous|| !mQueueEnabled) )
       return {Result::Status::kError, "already processing"};
     
     if (mThreadedTask)
-    {
-        mQueue.push_back(mHostParams);
         return Result();
-    }
-      
+    
     Result result;
-    mThreadedTask = std::unique_ptr<ThreadedTask>(new ThreadedTask(mClient,mHostParams, mSynchronous, result));
+    
+    if(mQueue.empty())
+      return {Result::Status::kWarning, "Process() called on empty queue"}; 
+    
+    mThreadedTask = std::unique_ptr<ThreadedTask>(new ThreadedTask(mClient,mQueue.front(), mSynchronous, result));
+    mQueue.pop_front();
     
     if (mSynchronous)
       mThreadedTask = nullptr;
@@ -499,6 +511,7 @@ public:
   {
     return mThreadedTask ? mThreadedTask->mState : kNoProcess;
   }
+  
   
 private:
     
@@ -614,7 +627,7 @@ private:
     bool mDetached = false;
   };
   
-  ParamSetType& mHostParams;
+  ParamSetType mHostParams;
   std::deque<ParamSetType> mQueue;
   bool mSynchronous = false;
   bool mQueueEnabled = false;
