5615522ab63a23f5cd4ab7772b0785dbcfc590bb
Owen Green <gungwho@gmail.com>
2019-11-15 20:00:55 +0000

    What was I smoking? Of course the params of a named object shouldn't be a global singleton, but should just be shared between objects with the same ID. ::sobs::

--- include/clients/nrt/FluidSharedInstanceAdaptor.hpp
+++ include/clients/nrt/FluidSharedInstanceAdaptor.hpp
@@ -8,6 +8,7 @@
 #include <data/TensorTypes.hpp>
 
 #include <string>
+#include <functional>
 #include <unordered_map>
 
 namespace fluid {
@@ -20,100 +21,148 @@ class ParamAliasAdaptor<NRTClient, std::tuple<Ts...>>
 {
   using WrappedClient = ClientWrapper<NRTClient>;
   using ParamSetType = typename WrappedClient::ParamSetType;
+  using ListenersArray = std::array<std::vector<std::function<void()>>,sizeof...(Ts)>;
+  
+  struct ListeningParams
+  {
+    ParamSetType params{ClientWrapper<NRTClient>::getParameterDescriptors()};
+    ListenersArray listeners;
+  };
 
+  using ParamsPointer = std::shared_ptr<ListeningParams>;
+  using LookupTable = std::unordered_map<std::string,ParamsPointer>;
+  
   template <size_t N>
   using ParamType = typename WrappedClient::ParamDescType::template ParamType<N>;
 
 public:
 
-  ParamAliasAdaptor(typename NRTClient::ParamDescType&){}
-
-
-  static typename WrappedClient::ParamSetType& instance()
+  ParamAliasAdaptor(typename NRTClient::ParamDescType&):mParams{new ListeningParams()}
+  {}
+  
+  ~ParamAliasAdaptor()
   {
-    return mParams;
+    if(mParams && mParams.use_count() == 2) //is this the last remaining user of this Corpus, except the hash table?
+    {
+      auto name = mParams->params.template get<0>();
+      mParamsTable.erase(name); //then remove it from the universe
+    }
+  }
+
+  typename WrappedClient::ParamSetType& instance()
+  {
+    return mParams->params;
   }
 
   auto keepConstrained(bool keep)
   {
-    return mParams.keepConstrained(keep);
+    return mParams->params.keepConstrained(keep);
   }
 
   std::array<Result, sizeof...(Ts)> constrainParameterValues()
   {
-    return mParams.constrainParameterValues();
+    return mParams->params.constrainParameterValues();
   }
 
   template <template <size_t N, typename T> class Func, typename... Args>
   std::array<Result, sizeof...(Ts)> setParameterValues(bool reportage, Args &&... args)
   {
-    return mParams.template setParameterValues<Func>(reportage, std::forward<Args>(args)...);
+    return mParams->params.template setParameterValues<Func>(reportage, std::forward<Args>(args)...);
   }
 
   template <template <size_t N, typename T> class Func, typename... Args>
   std::array<Result, sizeof...(Ts)> setFixedParameterValues(bool reportage, Args &&... args)
   {
-    return mParams.template setFixedParameterValues<Func>(reportage, std::forward<Args>(args)...);
+    auto results = mParams->params.template setFixedParameterValues<Func>(reportage, std::forward<Args>(args)...);
+    
+    std::string name = mParams->params.template get<0>();
+    
+    if(!name.size())
+    {
+      results[0].addMessage("Shared object given no name – won't be shared!");
+      results[0].set(Result::Status::kWarning);
+      return results;
+    }
+    
+    if(!mParamsTable.count(name))//key not already in table
+    {
+      mParamsTable.emplace(name, mParams);
+    }
+    else
+    {
+      //we won't override already set values, but we will complain
+      results[0].addMessage("Shared object called ", name, " already exists; ignoring new arguments");
+      results[0].set(Result::Status::kWarning);
+    }
+    
+    mParams = mParamsTable[name];
+    return results;
   }
 
   template <template <size_t N, typename T> class Func, typename... Args>
   std::array<Result, sizeof...(Ts)> setMutableParameterValues(bool reportage, Args &&... args)
   {
-    return mParams->template setMutableParameterValues<Func>(reportage, std::forward<Args>(args)...);
+    return mParams->params.template setMutableParameterValues<Func>(reportage, std::forward<Args>(args)...);
   }
 
   template <template <size_t N, typename T> class Func, typename... Args>
   void forEachParam(Args &&... args)
   {
-     mParams.template forEachParam<Func>(std::forward<Args>(args)...);
+     mParams->params.template forEachParam<Func>(std::forward<Args>(args)...);
   }
 
   template <typename T, template <size_t, typename> class Func, typename... Args>
   void forEachParamType(Args &&... args)
   {
-    mParams.template forEachParamType<T,Func>(std::forward<Args>(args)...);
+    mParams->params.template forEachParamType<T,Func>(std::forward<Args>(args)...);
   }
 
-  void reset() { mParams->reset(); }
+  void reset() { mParams->params.reset(); }
 
   template <size_t N>
   void set(typename ParamType<N>::type &&x, Result *reportage) noexcept
   {
-    mParams.template set<N>(std::forward<typename ParamType<N>::type>(x), reportage);
+    mParams->params.template set<N>(std::forward<typename ParamType<N>::type>(x), reportage);
 
-    auto listeners = mListeners[N];
+    auto listeners = mParams->listeners[N];
     for(auto&& l:listeners) l();
   }
 
   template <std::size_t N>
   auto &get() const
   {
-    return mParams.template get<N>();
+    return mParams->params.template get<N>();
   }
 
   template<size_t offset>
   auto subset()
   {
-    return mParams.template subset<offset>();
+    return mParams->params.template subset<offset>();
   }
 
   template<size_t N, typename F>
   void addListener(F&& f){
-    mListeners[N].emplace_back(std::forward<F>(f));
+     mParams->listeners[N].emplace_back(std::forward<F>(f));
   }
 
   private:
-    static typename WrappedClient::ParamSetType mParams;
-    static std::array<std::vector<std::function<void()>>,sizeof...(Ts)> mListeners;
+    ParamsPointer mParams;
+    static LookupTable mParamsTable;
+//    std::array<std::vector<std::function<void()>>,sizeof...(Ts)> mListeners;
 };
 
-template<typename NRTClient, typename...Ts> //init master param set
-typename ClientWrapper<NRTClient>::ParamSetType
-ParamAliasAdaptor<NRTClient, std::tuple<Ts...>>::mParams{ClientWrapper<NRTClient>::getParameterDescriptors()};
+//template<typename NRTClient, typename...Ts> //init master param set
+//typename ClientWrapper<NRTClient>::ParamSetType
+//ParamAliasAdaptor<NRTClient, std::tuple<Ts...>>::mParams{ClientWrapper<NRTClient>::getParameterDescriptors()};
 
-template<typename NRTClient, typename...Ts> //init parameter listeners
-std::array<std::vector<std::function<void()>>,sizeof...(Ts)>
-ParamAliasAdaptor<NRTClient, std::tuple<Ts...>>::mListeners{};
+//template<typename NRTClient, typename...Ts> //init parameter listeners
+//std::array<std::vector<std::function<void()>>,sizeof...(Ts)>
+//ParamAliasAdaptor<NRTClient, std::tuple<Ts...>>::mListeners{};
+
+template<typename NRTClient, typename...Ts>
+typename ParamAliasAdaptor<NRTClient, std::tuple<Ts...>>::LookupTable
+ParamAliasAdaptor<NRTClient, std::tuple<Ts...>>::mParamsTable{};
+
 
 template<typename NRTClient>
 class NRTSharedInstanceAdaptor : public OfflineIn, public OfflineOut
@@ -141,7 +190,7 @@ public:
   size_t audioBuffersIn()  const noexcept { return ParamDescType:: template NumOf<InputBufferT>();   }
   size_t audioBuffersOut() const noexcept { return ParamDescType:: template NumOf<BufferT>();  }
 
-  NRTSharedInstanceAdaptor(ParamSetType& p)
+  NRTSharedInstanceAdaptor(ParamSetType& p): mParams{p}
   {
       //Not using the nifty operator[] of unordered map, because it deault
     //constructs the value object, giving us shared_ptr<nullptr>
@@ -154,7 +203,7 @@ public:
   ~NRTSharedInstanceAdaptor()
   {
     if(mClient && mClient.use_count() == 2) //is this the last remaining user of this Corpus, except the hash table?
-      mClientTable.erase(ParamSetType::instance().template get<0>()); //then remove it from the universe
+      mClientTable.erase(mParams.get().instance().template get<0>()); //then remove it from the universe
   }
 
 
@@ -166,7 +215,7 @@ public:
   template<size_t N, typename T, typename...Args>
   decltype(auto) invoke(T&, Args&&...args)
   {
-    mProcessParams = ParamSetType::instance();
+    mProcessParams = mParams.get().instance();
     mClient->setParams(mProcessParams);
     return WrappedClient::getMessageDescriptors().template invoke<N>(*mClient, std::forward<Args>(args)...);
     //return mClient->template
@@ -175,7 +224,7 @@ public:
   template<typename T>
   Result process(FluidContext& c)
   {
-    mProcessParams = ParamSetType::instance();
+    mProcessParams = mParams.get().instance();
     mClient->setParams(mProcessParams);
     return mClient->template process<T>(c);
   }
@@ -183,6 +232,7 @@ public:
   void setParams(ParamSetType&) {}
 
 private:
+  std::reference_wrapper<ParamSetType> mParams;
   ClientPointer mClient;
   typename WrappedClient::ParamSetType mProcessParams{NRTClient::getParameterDescriptors()};
   static LookupTable mClientTable;
