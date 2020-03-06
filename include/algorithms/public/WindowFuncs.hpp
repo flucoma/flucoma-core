e116f2d6e10f1a9f878babd98b3dedf40438e4c2
Gerard <g.roma@hud.ac.uk>
2020-03-06 19:01:00 +0000

    WindowFuncs: convert int and size_t to index

--- include/algorithms/public/WindowFuncs.hpp
+++ include/algorithms/public/WindowFuncs.hpp
@@ -14,6 +14,7 @@ under the European Union’s Horizon 2020 research and innovation programme
 #include <cmath>
 #include <iostream>
 #include <map>
+#include "../../data/FluidIndex.hpp"
 
 namespace fluid {
 namespace algorithm {
@@ -24,37 +25,37 @@ public:
   enum class WindowTypes { kHann, kHamming, kBlackmanHarris, kGaussian };
   using WindowFuncsMap =
       std::map<WindowTypes,
-               std::function<void(int, Eigen::Ref<Eigen::ArrayXd>)>>;
+               std::function<void(index, Eigen::Ref<Eigen::ArrayXd>)>>;
 
   static WindowFuncsMap& map()
   {
     static WindowFuncsMap _funcs = {
         {WindowTypes::kHann,
-         [](int size, Eigen::Ref<Eigen::ArrayXd> out) {
-           for (int i = 0; i < size; i++)
+         [](index size, Eigen::Ref<Eigen::ArrayXd> out) {
+           for (index i = 0; i < size; i++)
            { out(i) = 0.5 - 0.5 * std::cos((M_PI * 2 * i) / size); }
          }},
         {WindowTypes::kHamming,
-         [](int size, Eigen::Ref<Eigen::ArrayXd> out) {
-           for (int i = 0; i < size; i++)
+         [](index size, Eigen::Ref<Eigen::ArrayXd> out) {
+           for (index i = 0; i < size; i++)
            { out(i) = 0.54 - 0.46 * std::cos((M_PI * 2 * i) / size); }
          }},
         {WindowTypes::kBlackmanHarris,
-         [](int size, Eigen::Ref<Eigen::ArrayXd> out) {
+         [](index size, Eigen::Ref<Eigen::ArrayXd> out) {
            using std::cos;
-           for (int i = 0; i < size; i++)
+           for (index i = 0; i < size; i++)
            {
              out(i) = 0.35875 - 0.48829 * cos((M_PI * 2 * i) / size) +
                       0.14128 * cos((M_PI * 2 * i) / size) +
                       0.01168 * cos((M_PI * 2 * i) / size);
            }
          }},
-        {WindowTypes::kGaussian, [](int size, Eigen::Ref<Eigen::ArrayXd> out) {
+        {WindowTypes::kGaussian, [](index size, Eigen::Ref<Eigen::ArrayXd> out) {
            using std::exp;
            double sigma = size / 3; // TODO: should be argument
            assert(size % 2);
-           int h = (size - 1) / 2;
-           for (int i = -h; i <= h; i++)
+           index h = (size - 1) / 2;
+           for (index i = -h; i <= h; i++)
            { out(i + h) = exp(-i * i / (2 * sigma * sigma)); }
          }}};
     return _funcs;
