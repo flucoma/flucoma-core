e703caa4ec2c2454b655a1f9daae6a21e5df3a0e
Owen Green <gungwho@gmail.com>
2020-03-12 15:03:44 +0000

    Version script falls back to commit hash if no tag has ever been set

--- script/flucoma_version.cmake
+++ script/flucoma_version.cmake
@@ -11,7 +11,7 @@ include_guard()
 find_package(Git REQUIRED)
 
 execute_process( 
-  COMMAND ${GIT_EXECUTABLE} describe --abbrev=0
+  COMMAND ${GIT_EXECUTABLE} describe --abbrev=0 --always
   RESULT_VARIABLE result
   OUTPUT_VARIABLE FLUID_VERSION_TAG
   OUTPUT_STRIP_TRAILING_WHITESPACE  
