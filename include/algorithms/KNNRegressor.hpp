ec21e456ea4aab3d05e706ecd2edf7839d9a3930
Gerard <g.roma@hud.ac.uk>
2020-04-21 16:15:20 +0100

    KNNRegressor: int to index

--- include/algorithms/KNNRegressor.hpp
+++ include/algorithms/KNNRegressor.hpp
@@ -16,12 +16,12 @@ class KNNRegressor {
 public:
 
   using DataSet = FluidDataSet<std::string, double, 1>;
-  double predict(KDTree tree, DataSet targets, RealVectorView point, int k) const{
+  double predict(KDTree tree, DataSet targets, RealVectorView point, index k) const{
     using namespace std;
     auto nearest = tree.kNearest(point, k);
     double prediction = 0;
     double weight = 1.0/k;
-    for(int i = 0; i < k; i++){
+    for(index i = 0; i < k; i++){
       auto id = nearest.getIds()(i);
       auto point = FluidTensor<double, 1>(1);
       targets.get(id, point);
