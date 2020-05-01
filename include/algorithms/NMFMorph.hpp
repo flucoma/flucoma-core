3e6eb0dfd8d2404586d2dd9302b605110a544cc4
Gerard <g.roma@hud.ac.uk>
2020-05-01 11:34:17 +0100

    adding NMFMorph

---
+++ include/algorithms/NMFMorph.hpp
@@ -0,0 +1,95 @@
+#pragma once
+
+#include "algorithms/OptimalTransport.hpp"
+#include "algorithms/RTPGHI.hpp"
+#include "algorithms/public/STFT.hpp"
+#include "algorithms/util/AlgorithmUtils.hpp"
+#include "algorithms/util/FluidEigenMappings.hpp"
+#include "algorithms/util/Munkres.hpp"
+#include "data/TensorTypes.hpp"
+#include <Eigen/Core>
+#include <Eigen/Dense>
+#include <vector>
+
+namespace fluid {
+namespace algorithm {
+
+class NMFMorph {
+
+public:
+  NMFMorph(index maxFFTSize) {}
+
+  void init(RealMatrixView W1, RealMatrixView W2, RealMatrixView H,
+            index winSize, index fftSize, index hopSize, bool assign) {
+    using namespace Eigen;
+    using namespace _impl;
+    mW1 = asEigen<Matrix>(W1).transpose();
+    mH = asEigen<Matrix>(H);
+    if (assign) {
+      MatrixXd tmpW2 = asEigen<Matrix>(W2).transpose();
+      ArrayXXd cost = ArrayXXd::Zero(mW1.cols(), tmpW2.cols());
+      for (index i = 0; i < mW1.cols(); i++) {
+        for (index j = 0; j < tmpW2.cols(); j++) {
+          ArrayXd tmp = mW1.col(i) - tmpW2.col(j);
+          cost(i, j) = std::sqrt(tmp.square().sum());
+        }
+      }
+      Munkres munk;
+      munk.init(mW1.cols(), tmpW2.cols());
+      ArrayXi result = ArrayXi::Zero(mW1.cols());
+      munk.process(cost, result);
+      mW2 = MatrixXd::Zero(tmpW2.rows(), tmpW2.cols());
+      for (index i = 0; i < result.size(); i++) {
+        mW2.col(i) = tmpW2.col(result(i));
+      }
+    } else {
+      mW2 = asEigen<Matrix>(W2).transpose();
+    }
+    mWindowSize = winSize;
+    mFFTSize = fftSize;
+    mHopSize = hopSize;
+    mSTFT = STFT(winSize, fftSize, hopSize);
+    mISTFT = ISTFT(winSize, fftSize, hopSize);
+    auto V = (mW2 * mH).transpose();
+    mRTPGHI.init(fftSize);
+
+    index rank = mW1.cols();
+    mOT = std::vector<OptimalTransport>(rank);
+    for (index i = 0; i < rank; i++) {
+      mOT[i].init(mW1.col(i), mW2.col(i));
+    }
+  }
+
+  void processFrame(ComplexVectorView v, double interpolation) {
+    using namespace Eigen;
+    using namespace _impl;
+    MatrixXd W = MatrixXd::Zero(mW1.rows(), mW1.cols());
+    for (int i = 0; i < W.cols(); i++) {
+      ArrayXd out = ArrayXd::Zero(mW2.rows());
+      mOT[i].interpolate(interpolation, out);
+      W.col(i) = out;
+    }
+
+    VectorXd hFrame = mH.col(mPos);
+    VectorXd frame = W * hFrame;
+    RealVectorView mag1 = asFluid(frame);
+    mRTPGHI.processFrame(mag1, v, mWindowSize, mFFTSize, mHopSize, 1e-6);
+    mPos = (mPos + 1) % mH.cols();
+  }
+
+private:
+  MatrixXd mW1;
+  MatrixXd mW2;
+  MatrixXd mH;
+  index mWindowSize;
+  index mHopSize;
+  index mFFTSize;
+  STFT mSTFT{1024, 1024, 512};
+  ISTFT mISTFT{1024, 1024, 512};
+  RTPGHI mRTPGHI;
+  std::vector<OptimalTransport> mOT;
+  int mPos{0};
+  int mIterations;
+};
+} // namespace algorithm
+} // namespace fluid
---
+++ include/algorithms/RTPGHI.hpp
@@ -0,0 +1,139 @@
+// Zdenek Prusa and Peter L. Soendergaard,
+// Real-time spectrogram inversion using phase gradient heap integration
+// Proceedings of DAFX 2016
+
+#pragma once
+
+#include "algorithms/public/STFT.hpp"
+#include "algorithms/util/AlgorithmUtils.hpp"
+#include "algorithms/util/FluidEigenMappings.hpp"
+#include "data/FluidIndex.hpp"
+#include "data/TensorTypes.hpp"
+#include <Eigen/Core>
+#include <cmath>
+
+namespace fluid {
+namespace algorithm {
+
+class RTPGHI {
+
+public:
+  using ArrayXd = Eigen::ArrayXd;
+  using ArrayXcd = Eigen::ArrayXcd;
+
+  void init(index fftSize) {
+    mBins = fftSize / 2 + 1;
+    mPrevMag = ArrayXd::Zero(mBins);
+    mPrevLogMag = ArrayXd::Zero(mBins);
+    mPrevPrevLogMag = ArrayXd::Zero(mBins);
+    mPrevPhase = ArrayXd::Zero(mBins);
+    mPrevPrevPhase = ArrayXd::Zero(mBins);
+    mPrevPhaseDeltaT = ArrayXd::Zero(mBins);
+    mBinIndices = ArrayXd::LinSpaced(mBins, 0, mBins - 1);
+  }
+
+  void processFrame(RealVectorView in, ComplexVectorView out, index winSize,
+                    index fftSize, index hopSize, double tolerance) {
+    using namespace Eigen;
+    using namespace _impl;
+    using namespace std;
+    using namespace std::complex_literals;
+
+    double gamma = 0.25645 * pow(winSize, 2); // assumes Hann window
+    ArrayXd mag = asEigen<Array>(in);
+    ArrayXd logMag = mag.max(epsilon).log();
+    ArrayXd futureLogMag = logMag;
+    ArrayXd currentLogMag = mPrevLogMag;
+    ArrayXd prevLogMag = mPrevPrevLogMag;
+
+    ArrayXd phaseDeltaT =
+        getPhaseDeltaT(currentLogMag, gamma, fftSize, hopSize);
+    ArrayXd phaseDeltaF =
+        getPhaseDeltaF(prevLogMag, futureLogMag, gamma, fftSize, hopSize);
+    double absTol =
+        log(tolerance) + max(currentLogMag.maxCoeff(), prevLogMag.maxCoeff());
+    ArrayXd todo = (currentLogMag > absTol).cast<double>();
+    index numTodo = todo.sum();
+    ArrayXd phaseEst = pi + ArrayXd::Random(mBins) * pi;
+
+    vector<pair<double, index>> heap;
+
+    for (index i = 0; i < mBins; i++) {
+      if (prevLogMag(i) > absTol)
+        heap.push_back({prevLogMag(i), i});
+    }
+    make_heap(heap.begin(), heap.end());
+
+    while (numTodo > 0 && heap.size() > 0) {
+      pop_heap(heap.begin(), heap.end());
+      index _m = heap.back().second;
+      heap.pop_back();
+
+      // use indices 0..mBins for prev frame
+      // mBins ... 2 * mBins  for current frame
+      if (_m < mBins && todo[_m] > 0) {
+        index m = _m;
+        phaseEst[m] =
+            mPrevPhase[m] + 0.5 * (phaseDeltaT[m] + mPrevPhaseDeltaT[m]);
+        heap.push_back({currentLogMag[m], m + mBins});
+        push_heap(heap.begin(), heap.end());
+        todo[m] = 0;
+        numTodo--;
+      } else if (_m >= mBins) {
+        index m = _m - mBins;
+        if (m < mBins - 1 && todo[m + 1] > 0) {
+          phaseEst[m + 1] =
+              phaseEst[m] + 0.5 * (phaseDeltaF[m] + phaseDeltaF[m + 1]);
+          heap.push_back({currentLogMag[m + 1], _m + 1});
+          push_heap(heap.begin(), heap.end());
+          todo[m + 1] = 0;
+          numTodo--;
+        }
+        if (m > 0 && todo[m - 1] > 0) {
+          phaseEst[m - 1] =
+              phaseEst[m] - 0.5 * (phaseDeltaF[m] + phaseDeltaF[m - 1]);
+          heap.push_back({currentLogMag[m - 1], _m - 1});
+          push_heap(heap.begin(), heap.end());
+          todo[m - 1] = 0;
+          numTodo--;
+        }
+      }
+    }
+    ArrayXd finalPhase =
+        phaseEst - ArrayXd::LinSpaced(mBins, 0, 1) * twoPi * hopSize;
+    ArrayXcd result = mPrevMag * (1i * finalPhase).exp();
+    mPrevPrevLogMag = mPrevLogMag;
+    mPrevLogMag = logMag;
+    mPrevPhase = phaseEst;
+    mPrevPhaseDeltaT = phaseDeltaT;
+    mPrevMag = mag;
+    out = asFluid(result);
+  }
+
+private:
+  Eigen::ArrayXd getPhaseDeltaT(Eigen::Ref<ArrayXd> logMag, double gamma,
+                                index fftSize, index hopSize) {
+    ArrayXd deltaT = ArrayXd::Zero(mBins);
+    deltaT.segment(1, mBins - 2) =
+        logMag.segment(2, mBins - 2) - logMag.segment(0, mBins - 2);
+    deltaT = deltaT * 0.5 * hopSize * fftSize / gamma;
+    deltaT = deltaT + twoPi * hopSize * mBinIndices / fftSize;
+    return deltaT;
+  }
+
+  Eigen::ArrayXd getPhaseDeltaF(ArrayXd prevLogMag, ArrayXd nextLogMag,
+                                double gamma, index fftSize, index hopSize) {
+    return 0.5 * (nextLogMag - prevLogMag) * (-gamma / (hopSize * fftSize));
+  }
+
+  index mBins;
+  ArrayXd mBinIndices;
+  ArrayXd mPrevMag;
+  ArrayXd mPrevLogMag;
+  ArrayXd mPrevPrevLogMag;
+  ArrayXd mPrevPhase;
+  ArrayXd mPrevPhaseDeltaT;
+  ArrayXd mPrevPrevPhase;
+};
+} // namespace algorithm
+} // namespace fluid
