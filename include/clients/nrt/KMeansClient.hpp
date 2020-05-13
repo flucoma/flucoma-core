781470c688a676cb3de79f3763e1467892734c65
Gerard <g.roma@hud.ac.uk>
2020-05-13 14:23:42 +0100

    KMeansClient interface change and refactoring

--- include/clients/nrt/KMeansClient.hpp
+++ include/clients/nrt/KMeansClient.hpp
@@ -12,144 +12,88 @@ class KMeansClient : public FluidBaseClient, OfflineIn, OfflineOut {
 public:
   using string = std::string;
   using BufferPtr = std::shared_ptr<BufferAdaptor>;
-
+  using IndexVector = FluidTensor<index, 1>;
+  using Labels = FluidDataSet<string, string, 1>;
   template <typename T> Result process(FluidContext &) { return {}; }
 
   FLUID_DECLARE_PARAMS();
 
-  KMeansClient(ParamSetViewType &p) : mParams(p) {
-    // mDims = get<kNDims>();
-    // mModel.init(mK, mDims);
+  KMeansClient(ParamSetViewType &p) : mParams(p) {}
+
+  MessageResult<IndexVector> fit(DataSetClientRef datasetClient, index k, index maxIter)
+    {
+    auto datasetClientPtr = datasetClient.get().lock();
+    if(!datasetClientPtr) return NoDataSetError;
+    auto dataSet = datasetClientPtr->getDataSet();
+    if (dataSet.size() == 0) return EmptyDataSetError;
+    if (k <= 1) return SmallKError;
+    if (maxIter <= 0) maxIter = 100;
+    mDims = dataSet.pointSize();
+    mK = k;
+    mModel.init(mK, mDims);
+    mModel.train(dataSet, maxIter);
+    IndexVector assignments(dataSet.size());
+    mModel.getAssignments(assignments);
+    return getCounts(assignments);
   }
 
-  MessageResult<FluidTensor<index, 1>> train(DataSetClientRef datasetClient,
-                                             index k, index maxIter,
-                                             BufferPtr init = nullptr) {
-    auto weakPtr = datasetClient.get();
-    FluidTensor<index, 1> counts(k);
-    counts.fill(0);
-    if (k <= 1)
-      return SmallKError;
-    if (maxIter <= 0)
-      maxIter = 100;
-    if (auto datasetClientPtr = weakPtr.lock()) {
+  MessageResult<IndexVector> fitPredict(
+    DataSetClientRef datasetClient,
+    LabelSetClientRef labelsetClient,
+    index k, index maxIter)
+  {
+      auto datasetClientPtr = datasetClient.get().lock();
+      if(!datasetClientPtr) return NoDataSetError;
       auto dataSet = datasetClientPtr->getDataSet();
-      if (dataSet.size() == 0)
-        return EmptyDataSetError;
+      if (dataSet.size() == 0) return EmptyDataSetError;
+      auto labelsetClientPtr = labelsetClient.get().lock();
+      if(!labelsetClientPtr) return NoLabelSetError;
+      if (k <= 1) return SmallKError;
+      if (maxIter <= 0) maxIter = 100;
+
       mDims = dataSet.pointSize();
       mK = k;
       mModel.init(mK, mDims);
-
-      if (init == nullptr) {
-        mModel.train(dataSet, maxIter);
-      } else {
-        BufferAdaptor::Access buf(init.get());
-        if (buf.numFrames() != mDims)
-          return WrongPointSizeError;
-        if (buf.numChans() != mK) {
-          return WrongInitError;
-        }
-        return NotImplementedError;
-
-        FluidTensor<double, 2> points(mDims, mK);
-        points = buf.samps(0, mDims, 0);
-        mModel.train(dataSet, maxIter, points);
-      }
-      //(const FluidDataSet<std::string, double, std::string, 1> &dataset, int
-      //maxIter,
-      //           RealMatrixView initialMeans = RealMatrixView(nullptr, 0, 0,
-      //           0))
-      FluidTensor<index, 1> assignments(dataSet.size());
+      mModel.train(dataSet, maxIter);
+      IndexVector assignments(dataSet.size());
       mModel.getAssignments(assignments);
-      for (auto a : assignments) {
-        counts[a]++;
-      }
-    } else {
-      return NoDataSetError;
-    }
-    return counts;
+      FluidTensorView<string, 1> ids = dataSet.getIds();
+      labelsetClientPtr->setLabelSet(getLabels(ids, assignments));
+      return getCounts(assignments);
   }
 
-  MessageResult<void> getClusters(DataSetClientRef datasetClient,
-                                  LabelSetClientRef labelClient) {
-    auto dataPtr = datasetClient.get().lock();
-    auto labelPtr = labelClient.get().lock();
-    if (!mModel.trained()) {
-      return NoDataFittedError;
-    }
-    // FluidTensor<intptr_t, 1> counts(mModel.getK());
-    // counts.fill(0);
-    if (dataPtr && labelPtr) {
+  MessageResult<IndexVector> predict(
+    DataSetClientRef datasetClient, LabelSetClientRef labelClient) const
+  {
+      auto dataPtr = datasetClient.get().lock();
+      if (!dataPtr) return NoDataSetError;
+      auto labelsetClientPtr = labelClient.get().lock();
+      if (!labelsetClientPtr) return NoLabelSetError;
       auto dataSet = dataPtr->getDataSet();
-      if (dataSet.size() == 0)
-        return EmptyDataSetError;
-      if (dataSet.size() != mModel.nAssigned())
-        return WrongPointNumError;
-      auto ids = dataSet.getIds();
-      FluidTensor<index, 1> assignments(dataSet.size());
-      mModel.getAssignments(assignments);
-      FluidDataSet<string, string, 1> result(1);
-      for (index i = 0; i < ids.size(); i++) {
-        index clusterId = assignments(i);
-        // counts(clusterId)++;
-        FluidTensor<string, 1> point = {std::to_string(clusterId)};
-        result.add(ids(i), point);
-      }
-      labelPtr->setLabelSet(result);
-    } else {
-      return NoDataSetOrLabelSetError;
+      if (dataSet.size() == 0) return EmptyDataSetError;
+      if (!mModel.trained()) return NoDataFittedError;
+      FluidTensorView<string, 1> ids = dataSet.getIds();
+      auto assignments = getAssignments(dataSet.size());
+      labelsetClientPtr->setLabelSet(getLabels(ids, assignments));
+      return getCounts(assignments);
+   }
+
+    MessageResult<index> predictPoint(BufferPtr data) const
+    {
+      if (!data) return NoBufferError;
+      if (!mModel.trained()) return NoDataFittedError;
+      BufferAdaptor::Access buf(data.get());
+      if (buf.numFrames() != mDims) return WrongPointSizeError;
+      FluidTensor<double, 1> point(mDims);
+      point = buf.samps(0, mDims, 0);
+      return mModel.vq(point);
     }
-    // return counts;
-    return {};
-  }
 
-  MessageResult<index> predictPoint(BufferPtr data) const {
-    if (!data)
-      return NoBufferError;
-    BufferAdaptor::Access buf(data.get());
-    if (!mModel.trained()) {
-      return NoDataFittedError;
-    }
-    if (buf.numFrames() != mDims)
-      return WrongPointSizeError;
-
-    FluidTensor<double, 1> point(mDims);
-    point = buf.samps(0, mDims, 0);
-    return mModel.vq(point);
-  }
-
-  MessageResult<FluidTensor<index, 1>>
-  predict(DataSetClientRef datasetClient, LabelSetClientRef labelClient) const {
-
-    auto dataPtr = datasetClient.get().lock();
-    if (!dataPtr) return NoDataSetError;
-    auto labelPtr = labelClient.get().lock();
-    if (!labelPtr) return NoLabelSetError;
-    auto dataSet = dataPtr->getDataSet();
-    if (dataSet.size() == 0) return EmptyDataSetError;
-    if (!mModel.trained()) return NoDataFittedError;
-
-    FluidTensor<index, 1> counts(mModel.getK());
-    counts.fill(0);
-    auto ids = dataSet.getIds();
-    FluidTensor<double, 1> query(mDims);
-    FluidDataSet<string, string, 1> result(1);
-    for (index i = 0; i < ids.size(); i++) {
-        dataSet.get(ids(i), query);
-        index clusterId = mModel.vq(query);
-        counts(clusterId)++;
-        FluidTensor<string, 1> point = {std::to_string(clusterId)};
-        result.add(ids(i), point);
-    }
-    labelPtr->setLabelSet(result);
-    return counts;
-  }
-
-  MessageResult<void> write(string fileName) {
+  MessageResult<void> write(string fileName)
+  {
     auto file = FluidFile(fileName, "w");
-    if (!file.valid()) {
-      return {Result::Status::kError, file.error()};
-    }
+    if (!file.valid())  return {Result::Status::kError, file.error()};
+
     RealMatrix means(mK, mDims);
     mModel.getMeans(means);
     file.add("means", means);
@@ -181,17 +125,47 @@ public:
 
   MessageResult<index> cols() { return mDims; }
 
-  FLUID_DECLARE_MESSAGES(makeMessage("train", &KMeansClient::train),
-                         makeMessage("fit", &KMeansClient::train),
+  FLUID_DECLARE_MESSAGES(makeMessage("fit", &KMeansClient::fit),
                          makeMessage("predict", &KMeansClient::predict),
                          makeMessage("predictPoint",
                                      &KMeansClient::predictPoint),
-                         makeMessage("getClusters", &KMeansClient::getClusters),
+                         makeMessage("fitPredict", &KMeansClient::fitPredict),
                          makeMessage("cols", &KMeansClient::cols),
                          makeMessage("write", &KMeansClient::write),
                          makeMessage("read", &KMeansClient::read));
 
 private:
+  IndexVector getCounts(IndexVector assignments) const{
+    IndexVector counts(mK);
+    counts.fill(0);
+    for (auto a : assignments) {
+        counts[a]++;
+    }
+    return counts;
+  }
+
+  IndexVector getAssignments(index N) const
+  {
+    IndexVector assignments(N);
+    FluidTensor<double, 1> query(mDims);
+    for (index i = 0; i < N; i++) {
+      assignments(i) = mModel.vq(query);
+    }
+    return assignments;
+  }
+
+  Labels getLabels(
+    FluidTensorView<string, 1>& ids,
+    IndexVector assignments) const
+  {
+    Labels result(1);
+    for (index i = 0; i < ids.size(); i++) {
+      FluidTensor<string, 1> point = {std::to_string(assignments(i))};
+      result.add(ids(i), point);
+    }
+    return result;
+  }
+
   algorithm::KMeans mModel;
   index mDims;
   index mK;
