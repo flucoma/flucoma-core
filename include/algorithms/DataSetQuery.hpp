ce2d1b5d84895e7397534a1fbd8d8555f7c0a024
Gerard <g.roma@hud.ac.uk>
2020-05-18 20:39:52 +0100

    add DataSetQuery

---
+++ include/algorithms/DataSetQuery.hpp
@@ -0,0 +1,129 @@
+#pragma once
+
+#include "algorithms/util/FluidEigenMappings.hpp"
+#include "data/FluidDataSet.hpp"
+#include "data/FluidTensor.hpp"
+#include "data/TensorTypes.hpp"
+#include "data/FluidIndex.hpp"
+#include <Eigen/Core>
+#include <set>
+#include <string>
+
+namespace fluid {
+namespace algorithm {
+
+class DataSetQuery {
+
+public:
+  using string = std::string;
+
+  using DataSet = FluidDataSet<string, double, 1>;
+
+  struct Condition{
+    index column;
+    index comparison;
+    double value;
+
+    bool test(RealVectorView point){
+      using namespace std;
+      switch(comparison){
+        case 0: return point(column) == value;
+        case 1: return point(column) != value;
+        case 2: return point(column) < value;
+        case 3: return point(column) <= value;
+        case 4: return point(column) > value;
+        case 5: return point(column) >= value;
+      }
+    }
+  };
+
+  DataSetQuery()
+  {
+    mComparisons = std::set<std::string>{"==", "!=", "<", "<=", ">", ">="};
+  }
+
+  void addColumn(index col)
+  {
+    mColumns.emplace(col);
+  }
+
+  void addRange(index from, index to)
+  {
+    for(index i = from; i < to; i++){
+      mColumns.emplace(i);
+    }
+  }
+
+  index numColumns()
+  {
+    return mColumns.size();
+  }
+
+  bool hasAndConditions()
+  {
+    return mAndConditions.size() > 0;
+  }
+
+  index maxColumn()
+  {
+    return mColumns.empty()? 0: *mColumns.rbegin();
+  }
+
+  bool addCondition(index column, string comparison, double value, bool conjunction)
+  {
+    auto pos = mComparisons.find(comparison);
+    if(pos == mComparisons.end()) return false;
+    index i = std::distance(mComparisons.begin(), pos);
+    if(conjunction) mAndConditions.emplace_back(Condition{column, i, value});
+    else mOrConditions.emplace_back(Condition{column, i, value});
+    return true;
+  }
+
+  void process (const DataSet& input, DataSet& output){
+    auto data = input.getData();
+    auto ids = input.getIds();
+    for(index i = 0; i < input.size(); i++){
+       bool matchesAllAnd = true;
+       auto point = data.row(i);
+       for(index j = 0; j < mAndConditions.size(); j++)
+         if(!mAndConditions[j].test(point)) matchesAllAnd = false;
+       if (matchesAllAnd) {
+         addRow(ids(i), point, output);
+         continue;
+       }
+       else{
+         bool matchesAnyOr = false;
+         for(index k = 0; k < mOrConditions.size(); k++)
+         if(mOrConditions[k].test(point)) matchesAnyOr = true;
+         if (matchesAnyOr) {
+           addRow(ids(i), point, output);
+         }
+     }
+   }
+ }
+
+  void print() const { }
+
+  void reset() {
+    mColumns.clear();
+    mAndConditions.clear();
+    mOrConditions.clear();
+   }
+
+private:
+
+  void addRow(string id, RealVectorView point, DataSet& out){
+    RealVector newPoint(mColumns.size());
+    newPoint.fill(0);
+    index i = 0;
+    for(auto c: mColumns) newPoint(i++) = point(c);
+    out.add(id, newPoint);
+  }
+
+  std::set<index> mColumns;
+  std::set<std::string> mComparisons;
+  std::vector<Condition> mAndConditions;
+  std::vector<Condition> mOrConditions;
+};
+} // namespace algorithm
+} // namespace fluid
---
+++ include/clients/nrt/DatasetQueryClient.hpp
@@ -0,0 +1,93 @@
+#pragma once
+#include "NRTClient.hpp"
+#include "algorithms/DataSetQuery.hpp"
+
+namespace fluid {
+namespace client {
+
+class DataSetQueryClient : public FluidBaseClient, OfflineIn, OfflineOut {
+  enum { kName };
+
+public:
+  using string = std::string;
+  using BufferPtr = std::shared_ptr<BufferAdaptor>;
+  using DataSet = FluidDataSet<string, double, 1>;
+
+  template <typename T> Result process(FluidContext &) { return {}; }
+
+  FLUID_DECLARE_PARAMS();
+
+  DataSetQueryClient(ParamSetViewType &p) : mParams(p) {}
+
+  MessageResult<void> addColumn(index column) {
+    if (column < 0) return Error("invalid index");
+    mAlgorithm.addColumn(column);
+    return OK();
+  }
+  MessageResult<void> addRange(index from, index to) {
+    if (from < 0 || to  < 0 || to < from) return Error("invalid range");
+    mAlgorithm.addRange(from, to);
+    return OK();
+  }
+
+  MessageResult<void> filter(index column, string comparison, double value) {
+    if (column < 0) return Error("invalid index");
+    if(mAlgorithm.hasAndConditions())Error("Filter already set");
+    bool result = mAlgorithm.addCondition(column, comparison, value ,true);
+    if(!result)return Error("invalid filter");
+    else return OK();
+  }
+
+  MessageResult<void> andFilter(index column, string comparison, double value) {
+    if (column < 0) return Error("invalid index");
+    if(!mAlgorithm.hasAndConditions())Error("Add a filter first");
+    bool result = mAlgorithm.addCondition(column, comparison, value, true);
+    if(!result)return Error("invalid filter");
+    else return OK();
+  }
+
+  MessageResult<void> orFilter(index column, string comparison, double value) {
+    if (column < 0) return Error("invalid index");
+    if(!mAlgorithm.hasAndConditions())Error("Add a filter first");
+    bool result = mAlgorithm.addCondition(column, comparison, value, false);
+    if(!result)return Error("invalid filter");
+    else return OK();
+  }
+
+
+  MessageResult<void> transform(DataSetClientRef sourceClient, DataSetClientRef destClient) {
+    if(mAlgorithm.numColumns() <= 0) return Error("No columns");
+    auto srcPtr = sourceClient.get().lock();
+    auto destPtr = destClient.get().lock();
+    if(!srcPtr || !destPtr) return Error(NoDataSet);
+    auto src = srcPtr->getDataSet();
+    if (src.size() == 0) return Error(EmptyDataSet);
+    if(src.pointSize() < mAlgorithm.maxColumn()) return Error(WrongPointSize);
+    FluidDataSet<string, double, 1> result(mAlgorithm.numColumns());
+    mAlgorithm.process(src, result);
+    destPtr->setDataSet(result);
+    return OK();
+  }
+
+  MessageResult<void> reset() {
+    mAlgorithm.reset();
+    return OK();
+  }
+
+
+  FLUID_DECLARE_MESSAGES(
+                        makeMessage("transform", &DataSetQueryClient::transform),
+                         makeMessage("addColumn", &DataSetQueryClient::addColumn),
+                         makeMessage("addRange", &DataSetQueryClient::addRange),
+                         makeMessage("filter", &DataSetQueryClient::filter),
+                         makeMessage("and", &DataSetQueryClient::andFilter),
+                         makeMessage("or", &DataSetQueryClient::orFilter),
+                         makeMessage("reset", &DataSetQueryClient::reset)
+  );
+
+private:
+  algorithm::DataSetQuery mAlgorithm;
+};
+using NRTThreadedDataSetQueryClient = NRTThreadingAdaptor<ClientWrapper<DataSetQueryClient>>;
+} // namespace client
+} // namespace fluid
