074dc169be3fc3f02bedf73f3bd801d3e34c55f3
Gerard <g.roma@hud.ac.uk>
2020-07-08 18:37:22 +0100

    KNNRegressor adapted to signal input

--- include/clients/nrt/KNNRegressorClient.hpp
+++ include/clients/nrt/KNNRegressorClient.hpp
@@ -1,39 +1,39 @@
 #pragma once
 
-#include "NRTClient.hpp"
 #include "DataSetClient.hpp"
+#include "NRTClient.hpp"
 #include "algorithms/KNNRegressor.hpp"
 
 namespace fluid {
 namespace client {
 
-  struct KNNRegressorData{
-    algorithm::KDTree tree{0};
-    FluidDataSet<std::string, double, 1> target{1};
-    index size(){return target.size();}
-    index dims(){return tree.dims();}
-  };
+struct KNNRegressorData {
+  algorithm::KDTree tree{0};
+  FluidDataSet<std::string, double, 1> target{1};
+  index size() { return target.size(); }
+  index dims() { return tree.dims(); }
+};
 
-  void to_json(nlohmann::json& j, const KNNRegressorData& data) {
-    j["tree"] = data.tree;
-    j["target"] = data.target;
-  }
+void to_json(nlohmann::json &j, const KNNRegressorData &data) {
+  j["tree"] = data.tree;
+  j["target"] = data.target;
+}
 
-  bool check_json(const nlohmann::json& j, const KNNRegressorData&){
-    return fluid::check_json(j,
-      {"tree", "target"}, {JSONTypes::OBJECT, JSONTypes::OBJECT}
-    );
-  }
+bool check_json(const nlohmann::json &j, const KNNRegressorData &) {
+  return fluid::check_json(j, {"tree", "target"},
+                           {JSONTypes::OBJECT, JSONTypes::OBJECT});
+}
 
-  void from_json(const nlohmann::json& j, KNNRegressorData& data) {
-    data.tree = j["tree"].get<algorithm::KDTree>();
-    data.target = j["target"].get<FluidDataSet<std::string, double, 1>>();
-  }
+void from_json(const nlohmann::json &j, KNNRegressorData &data) {
+  data.tree = j["tree"].get<algorithm::KDTree>();
+  data.target = j["target"].get<FluidDataSet<std::string, double, 1>>();
+}
 
-class KNNRegressorClient : public FluidBaseClient, OfflineIn, OfflineOut, ModelObject,
-  public DataClient<KNNRegressorData> {
-
-  enum { kNDims, kK };
+class KNNRegressorClient : public FluidBaseClient,
+                           AudioIn,
+                           ControlOut,
+                           ModelObject,
+                           public DataClient<KNNRegressorData> {
 
 public:
   using string = std::string;
@@ -41,62 +41,103 @@ public:
   using DataSet = FluidDataSet<string, double, 1>;
   using StringVector = FluidTensor<string, 1>;
 
-  template <typename T> Result process(FluidContext &) { return {}; }
+  enum { kNumNeighbors, kInputBuffer, kOutputBuffer };
+  FLUID_DECLARE_PARAMS(
+    LongParam("numNeighbors","Number of nearest neighbors", 3),
+    BufferParam("inputPointBuffer","Input Point Buffer"),
+    BufferParam("predictionBuffer","Prediction Buffer")
+  );
 
-  FLUID_DECLARE_PARAMS();
+  KNNRegressorClient(ParamSetViewType &p) : mParams(p) {
+    audioChannelsIn(1);
+    controlChannelsOut(1);
+  }
 
-  KNNRegressorClient(ParamSetViewType &p) : mParams(p) {}
+  template <typename T>
+  void process(std::vector<FluidTensorView<T, 1>> &input,
+               std::vector<FluidTensorView<T, 1>> &output, FluidContext &)
+  {
+    index k = get<kNumNeighbors>();
+    if(k == 0 || mTree.size() == 0 || mTree.size() < k) return;
+    InOutBuffersCheck bufCheck(mTree.dims());
+    if(!bufCheck.checkInputs(
+      get<kInputBuffer>().get(),
+      get<kOutputBuffer>().get()))
+      return;
+    algorithm::KNNRegressor regressor;
+    RealVector point(mTree.dims());
+    point = bufCheck.in().samps(0, mTree.dims(), 0);
+    mTrigger.process(input, output, [&](){
+      double result = regressor.predict(mTree, mTarget, point, k, true);
+      bufCheck.out().samps(0)[0] = result;
+    });
+  }
 
-  MessageResult<string> fit(
-    DataSetClientRef datasetClient,
-    DataSetClientRef targetClient)
-    {
+  index latency() { return 0; }
+
+  MessageResult<string> fit(DataSetClientRef datasetClient,
+                            DataSetClientRef targetClient) {
     auto datasetClientPtr = datasetClient.get().lock();
-    if(!datasetClientPtr) return Error<string>(NoDataSet);
+    if (!datasetClientPtr)
+      return Error<string>(NoDataSet);
     auto dataSet = datasetClientPtr->getDataSet();
-    if (dataSet.size() == 0) return Error<string>(EmptyDataSet);
+    if (dataSet.size() == 0)
+      return Error<string>(EmptyDataSet);
     auto targetClientPtr = targetClient.get().lock();
-    if(!targetClientPtr) return Error<string>(NoDataSet);
+    if (!targetClientPtr)
+      return Error<string>(NoDataSet);
     auto target = targetClientPtr->getDataSet();
-    if (target.size() == 0) return Error<string>(EmptyDataSet);
-    if(dataSet.size() != target.size())return Error<string>(SizesDontMatch);
+    if (target.size() == 0)
+      return Error<string>(EmptyDataSet);
+    if (dataSet.size() != target.size())
+      return Error<string>(SizesDontMatch);
     mTree = algorithm::KDTree{dataSet};
     mTarget = target;
     mAlgorithm = {mTree, mTarget};
     return {};
   }
 
-  MessageResult<double> predictPoint(
-    BufferPtr data, fluid::index k, bool uniform) const
-    {
+  MessageResult<double> predictPoint(BufferPtr data, fluid::index k,
+                                     bool uniform) const {
     algorithm::KNNRegressor regressor;
-    if (!data) return Error<double>(NoBuffer);
-    if(k == 0) return Error<double>(SmallK);
-    if(mTree.size() == 0) return Error<double>(NoDataFitted);
-    if (mTree.size() < k) return Error<double>(NotEnoughData);
+    if (!data)
+      return Error<double>(NoBuffer);
+    if (k == 0)
+      return Error<double>(SmallK);
+    if (mTree.size() == 0)
+      return Error<double>(NoDataFitted);
+    if (mTree.size() < k)
+      return Error<double>(NotEnoughData);
     BufferAdaptor::Access buf(data.get());
-    if(!buf.exists()) return Error<double>(InvalidBuffer);
-    if (buf.numFrames() != mTree.dims()) return Error<double>(WrongPointSize);
+    if (!buf.exists())
+      return Error<double>(InvalidBuffer);
+    if (buf.numFrames() != mTree.dims())
+      return Error<double>(WrongPointSize);
     RealVector point(mTree.dims());
     point = buf.samps(0, mTree.dims(), 0);
     double result = regressor.predict(mTree, mTarget, point, k, !uniform);
     return result;
   }
 
-  MessageResult<void> predict(
-    DataSetClientRef source,
-    DataSetClientRef dest, fluid::index k, bool uniform) const
-    {
+  MessageResult<void> predict(DataSetClientRef source, DataSetClientRef dest,
+                              fluid::index k, bool uniform) const {
     auto sourcePtr = source.get().lock();
-    if(!sourcePtr) return Error(NoDataSet);
+    if (!sourcePtr)
+      return Error(NoDataSet);
     auto dataSet = sourcePtr->getDataSet();
-    if (dataSet.size() == 0) return Error(EmptyDataSet);
+    if (dataSet.size() == 0)
+      return Error(EmptyDataSet);
     auto destPtr = dest.get().lock();
-    if(!destPtr) return Error(NoDataSet);
-    if (dataSet.pointSize()!=mTree.dims()) return Error(WrongPointSize);
-    if(k == 0) return Error(SmallK);
-    if(mTree.size() == 0) return Error(NoDataFitted);
-    if (mTree.size() < k) return Error(NotEnoughData);
+    if (!destPtr)
+      return Error(NoDataSet);
+    if (dataSet.pointSize() != mTree.dims())
+      return Error(WrongPointSize);
+    if (k == 0)
+      return Error(SmallK);
+    if (mTree.size() == 0)
+      return Error(NoDataFitted);
+    if (mTree.size() < k)
+      return Error(NotEnoughData);
 
     algorithm::KNNRegressor regressor;
     auto ids = dataSet.getIds();
@@ -105,39 +146,31 @@ public:
     for (index i = 0; i < dataSet.size(); i++) {
       RealVectorView point = data.row(i);
       RealVector prediction = {
-        regressor.predict(mTree, mTarget, point, k, !uniform)
-      };
+          regressor.predict(mTree, mTarget, point, k, !uniform)};
       result.add(ids(i), prediction);
     }
     destPtr->setDataSet(result);
     return OK();
   }
 
-  FLUID_DECLARE_MESSAGES(makeMessage("fit",
-                         &KNNRegressorClient::fit),
-                         makeMessage("predict",
-                         &KNNRegressorClient::predict),
+  FLUID_DECLARE_MESSAGES(makeMessage("fit", &KNNRegressorClient::fit),
+                         makeMessage("predict", &KNNRegressorClient::predict),
                          makeMessage("predictPoint",
-                         &KNNRegressorClient::predictPoint),
-                         makeMessage("cols",
-                         &KNNRegressorClient::dims),
-                         makeMessage("size",
-                         &KNNRegressorClient::size),
-                         makeMessage("load",
-                         &KNNRegressorClient::load),
-                         makeMessage("dump",
-                         &KNNRegressorClient::dump),
-                         makeMessage("write",
-                         &KNNRegressorClient::write),
-                         makeMessage("read",
-                         &KNNRegressorClient::read)
-                       );
+                                     &KNNRegressorClient::predictPoint),
+                         makeMessage("cols", &KNNRegressorClient::dims),
+                         makeMessage("size", &KNNRegressorClient::size),
+                         makeMessage("load", &KNNRegressorClient::load),
+                         makeMessage("dump", &KNNRegressorClient::dump),
+                         makeMessage("write", &KNNRegressorClient::write),
+                         makeMessage("read", &KNNRegressorClient::read));
+
 private:
   algorithm::KDTree mTree{0};
   DataSet mTarget{1};
+  FluidInputTrigger mTrigger;
 };
 
-using NRTThreadedKNNRegressorClient = NRTThreadingAdaptor<ClientWrapper<KNNRegressorClient>>;
+using RTKNNRegressorClient = ClientWrapper<KNNRegressorClient>;
 
 } // namespace client
 } // namespace fluid
