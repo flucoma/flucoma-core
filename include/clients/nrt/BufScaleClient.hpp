dd09364858627cb72241eac113e6467a9692dfa6
Pierre Alexandre Tremblay <info@pierrealexandretremblay.com>
2020-09-16 13:32:11 +0100

    bufscale now has rect input too

--- include/clients/nrt/BufScaleClient.hpp
+++ include/clients/nrt/BufScaleClient.hpp
@@ -27,6 +27,10 @@ class BufScaleClient :    public FluidBaseClient,
 
   enum BufSelectParamIndex {
     kSource,
+    kStartFrame,
+    kNumFrames,
+    kStartChan,
+    kNumChans,
     kDest, 
     kInLow,
     kInHigh,
@@ -36,6 +40,10 @@ class BufScaleClient :    public FluidBaseClient,
   public:
   
   FLUID_DECLARE_PARAMS(InputBufferParam("source", "Source Buffer"),
+                       LongParam("startFrame", "Source Offset", 0, Min(0)),
+                       LongParam("numFrames", "Number of Frames", -1),
+                       LongParam("startChan", "Start Channel", 0, Min(0)),
+                       LongParam("numChans", "Number of Channels", -1),
                        BufferParam("destination","Destination Buffer"), 
                        FloatParam("inputLow","Input Low Range",0),
                        FloatParam("inputHigh","Input High Range",1),
@@ -66,23 +74,47 @@ class BufScaleClient :    public FluidBaseClient,
     if (!dest.exists())
       return {Result::Status::kError, "Output buffer not found"};
     
+      // retrieve the range requested and check it is valid
+      index startFrame = get<kStartFrame>(); 
+      index startChan  = get<kStartChan>();
+      index numFrames = get<kNumFrames>() == -1
+                            ? (source.numFrames() - get<kStartFrame>())
+                            : get<kNumFrames>();
+      index numChans = get<kNumChans>() == -1
+                              ? (source.numChans() - get<kStartChan>())
+                              : get<kNumChans>();
+
+      if (get<kStartFrame>() + numFrames > source.numFrames())
+        return {Result::Status::kError, "Start frame + num frames (",
+                get<kStartFrame>() + get<kNumFrames>(), ") out of range."};
+
+      if (get<kStartChan>() + numChans > source.numChans())
+        return {Result::Status::kError, "Start channel ", get<kStartChan>(),
+                " out of range."};
+
+      if (numChans <= 0 || numFrames <= 0)
+        return {Result::Status::kError, "Zero length segment requested"};
+      // import the data to be processed in a temp Tensor
+      
+      FluidTensor<float, 2> tmp(numChans,numFrames);
+    
+      for(index i = 0; i < numChans; ++i)
+        tmp.row(i) = source.samps(startFrame, numFrames, (i + startChan));
+        
+    //process
     double scale = (get<kOutHigh>()-get<kOutLow>())/(get<kInHigh>()-get<kInLow>()); 
     double offset = get<kOutLow>() - ( scale * get<kInLow>() ); 
-
-    FluidTensor<float, 2> tmp(source.numFrames(),source.numChans());
-  
-    for(index i = 0; i < source.numChans(); ++i)
-      tmp.col(i) = source.samps(i);
     
     tmp.apply([&offset,&scale](float& x){
         x *= scale;
         x += offset; 
     }); 
-        
-    dest.resize(source.numFrames(),source.numChans(),source.sampleRate());
+
+    //write back the processed data, resizing the dest buffer          
+    dest.resize(numFrames,numChans,source.sampleRate());
     
-    for(index i = 0; i < source.numChans(); ++i)
-       dest.samps(i) = tmp.col(i);
+    for(index i = 0; i < numChans; ++i)
+       dest.samps(i) = tmp.row(i);
     
     return {};
   }                  
