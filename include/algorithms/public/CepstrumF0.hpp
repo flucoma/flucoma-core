4cfc2f45734c9c7374ffb8946c1d8cabf7e9994f
Gerard <g.roma@hud.ac.uk>
2021-05-14 18:28:31 +0100

    CepstrumF0: initialize pitch to 0, fix condition for freque ncy range, fix peak detection

--- include/algorithms/public/CepstrumF0.hpp
+++ include/algorithms/public/CepstrumF0.hpp
@@ -49,16 +49,17 @@ public:
 
     ArrayXd mag = _impl::asEigen<Array>(input);
     ArrayXd logMag = mag.max(epsilon).log();
-    double  pitch = sampleRate / minFreq;
+    double  pitch = 0;
     double  confidence = 0;
-    index   minBin = std::lrint(sampleRate / maxFreq);
-    index   maxBin = std::lrint(sampleRate / minFreq);
+    index   minBin = min(lrint(sampleRate / maxFreq),  mag.size());
+    index   maxBin = min(lrint(sampleRate / minFreq), mag.size());
 
     mDCT.processFrame(logMag, mCepstrum);
 
-    if (maxBin > minBin && maxBin < mCepstrum.size())
+    if (maxBin > minBin)
     {
-      auto vec = pd.process(mCepstrum.segment(minBin, maxBin - minBin), 1);
+      auto seg = mCepstrum.segment(minBin, maxBin - minBin);
+      auto vec = pd.process(mCepstrum.segment(minBin, maxBin - minBin), 1, seg.minCoeff());
       if (vec.size() > 0)
       {
         pitch = sampleRate / (vec[0].first + minBin);
