94050dae8239329e32c86077e0c3b07c32853b89
Gerard <g.roma@hud.ac.uk>
2021-05-31 16:45:20 +0100

    PitchClient: output -999 instead of 0 for MIDI cents

--- include/clients/rt/PitchClient.hpp
+++ include/clients/rt/PitchClient.hpp
@@ -118,7 +118,7 @@ public:
         });
     // pitch
     if(get<kUnit>() == 1){
-      output[0](0) = mDescriptors(0) == 0? 0:
+      output[0](0) = mDescriptors(0) == 0? -999:
       69 + (12 * log2(mDescriptors(0) / 440.0));
     }
     else {
