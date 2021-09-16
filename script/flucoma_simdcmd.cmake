1865354d6aea2a5b3434d5779cdaf7bd27dadde7
Owen Green <gungwho@gmail.com>
2021-09-16 08:26:02 +0100

    No explicit switches needed for SSE  + x64 on Apple / MSVC. Leave them for Linux

--- script/flucoma_simdcmd.cmake
+++ script/flucoma_simdcmd.cmake
@@ -8,19 +8,23 @@
 
 include_guard()
 
-if(CMAKE_SYSTEM_PROCESSOR MATCHES "amd64.*|x86_64.*|AMD64.*|i686.*|i386.*|x86.*")
-  if(MSVC)
+if(APPLE)
+    # https://stackoverflow.com/a/45921250
+    set (SIMD_OPT -march=core2 -mtune=haswell)  
+  elseif(MSVC)
     if(CMAKE_SIZEOF_VOID_P EQUAL 4) #32bit; SSE2 is always on for x64 MSVC
       set(SIMD_OPT /arch:SSE2)
     endif()
+  elseif(${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
+    if(CMAKE_SYSTEM_PROCESSOR MATCHES "amd64.*|x86_64.*|AMD64.*|i686.*|i386.*|x86.*")
+        set(SIMD_OPT -msse2)
+    elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "arm")   
+       set(SIMD_OPT -march=armv7-a -mtune=cortex-a8 -mfloat-abi=hard -mfpu=neon)
+    endif() 
   else()
-    set(SIMD_OPT -msse2)
-  endif() 
-elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "arm")   
-   set(SIMD_OPT -march=armv7-a -mtune=cortex-a8 -mfloat-abi=hard -mfpu=neon) 
-else() 
-    message(WARNING "Don't know about ${CMAKE_SYSTEM_PROCESSOR} type: if you know the compiler flag for enabling vector instructions, please pass this to CMake with -DFLUID_ARCH")
-endif()
+      message(WARNING "Don't know about ${CMAKE_SYSTEM_PROCESSOR} type: if you know the compiler flag for enabling vector instructions, please pass this to CMake with -DFLUID_ARCH")
+  endif()
+endif() 
 
 if(SIMD_OPT)
   set(FLUID_ARCH ${SIMD_OPT} CACHE STRING "Flag for using vector instruction sets")
