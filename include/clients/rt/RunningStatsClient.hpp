989d06b5a0ce0158e85aa4f76cc9db9204cb00b2
Owen Green <gungwho@gmail.com>
2021-09-17 01:07:58 +0100

    Change to a rolling algorithm, less biased variance estimator

--- include/clients/rt/RunningStatsClient.hpp
+++ include/clients/rt/RunningStatsClient.hpp
@@ -10,12 +10,13 @@ under the European Union’s Horizon 2020 research and innovation programme
 
 #pragma once
 
-#include "../../algorithms/public/IncrementalStats.hpp"
-#include "../common/FluidBaseClient.hpp"
 #include "../common/AudioClient.hpp"
+#include "../common/FluidBaseClient.hpp"
+#include "../common/FluidSource.hpp"
 #include "../common/ParameterConstraints.hpp"
 #include "../common/ParameterSet.hpp"
 #include "../common/ParameterTypes.hpp"
+#include "../../algorithms/public/RunningStats.hpp"
 #include "../../data/TensorTypes.hpp"
 
 namespace fluid {
@@ -25,20 +26,10 @@ namespace runningstats {
 template <typename T>
 using HostVector = FluidTensorView<T, 1>;
 
-//enum PitchParamIndex {
-//  kAlgorithm,
-//  kMinFreq,
-//  kMaxFreq,
-//  kUnit,
-//  kFFT,
-//  kMaxFFTSize
-//};
+constexpr auto RunningStatsParams =
+    defineParameters(LongParam("size", "History Size", 2, Min(2)));
 
-constexpr auto RunningStatsParams = defineParameters(
-  LongParam("size","History Size",2,Min(1))
-);
-
-class RunningStatsClient : public FluidBaseClient,  public ControlIn,  ControlOut
+class RunningStatsClient : public FluidBaseClient, public ControlIn, ControlOut
 {
 public:
   using ParamDescType = decltype(RunningStatsParams);
@@ -54,53 +45,58 @@ public:
     return mParams.get().template get<N>();
   }
 
-  static constexpr auto& getParameterDescriptors() 
-  { 
-    return RunningStatsParams; 
+  static constexpr auto& getParameterDescriptors()
+  {
+    return RunningStatsParams;
   }
 
-  RunningStatsClient(ParamSetViewType& p): mParams(p)
+  RunningStatsClient(ParamSetViewType& p) : mParams(p)
   {
     controlChannelsIn(1);
-    controlChannelsOut({2,-1}	);
+    controlChannelsOut({2, -1});
     setInputLabels({"list input"});
-    setOutputLabels({"mean","standard deviation"});
-  } 
-  
+    setOutputLabels({"mean", "sample standard deviation"});
+  }
+
   template <typename T>
   void process(std::vector<HostVector<T>>& input,
-               std::vector<HostVector<T>>& output, FluidContext& c)
+               std::vector<HostVector<T>>& output, FluidContext&)
   {
-    mAlgorithm.process(input[0],output[0],output[1]); 
+
+    bool inputSizeChanged = inputSize != input[0].size() ;
+    bool sizeParamChanged = mSizeTracker.changed(get<0>());
+
+    if(inputSizeChanged|| sizeParamChanged)
+    {
+      inputSize = input[0].size();
+      mAlgorithm.init(get<0>(),inputSize);
+    }
+
+    mAlgorithm.process(input[0],output[0],output[1]);
   }
-  
-  MessageResult<void> clear() { mAlgorithm.clear(); return {}; }
-  
+
+  MessageResult<void> clear()
+  {     
+    mAlgorithm.init(get<0>(),inputSize);
+    return {};
+  }
+
   static auto getMessageDescriptors()
   {
-    return defineMessages(
-        makeMessage("clear", &RunningStatsClient::clear)
-    );
+    return defineMessages(makeMessage("clear", &RunningStatsClient::clear));
   }
-  
+
   index latency() { return 0; }
+
 private:
-  algorithm::IncrementalStats mAlgorithm; 
+  algorithm::RunningStats mAlgorithm;
+  index inputSize{0};
+  ParameterTrackChanges<index> mSizeTracker;
 };
 
 } // namespace runningstats
 
 using RunningStatsClient = ClientWrapper<runningstats::RunningStatsClient>;
-
-//auto constexpr NRTPitchParams = makeNRTParams<runningstats::RunningStatsClient>(
-//    InputBufferParam("source", "Source Buffer"),
-//    BufferParam("features", "Features Buffer"));
-//
-//using NRTPitchClient =
-//    NRTControlAdaptor<pitch::PitchClient, decltype(NRTPitchParams),
-//                      NRTPitchParams, 1, 1>;
-//
-//using NRTThreadedPitchClient = NRTThreadingAdaptor<NRTPitchClient>;
 
 } // namespace client
 } // namespace fluid
