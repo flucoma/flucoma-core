af29cabec46d6a2e8763f562f87f8a4e0c5d09b8
Alex Harker <ajharker@gmail.com>
2022-05-31 18:59:25 +0100

    Attempt to correct novelty object latency reporting

--- include/clients/rt/NoveltyFeatureClient.hpp
+++ include/clients/rt/NoveltyFeatureClient.hpp
@@ -179,7 +179,7 @@ public:
     index filterSize = get<kFilterSize>();
     if (filterSize % 2) filterSize++;
     return get<kFFT>().hopSize() *
-           (1 + ((get<kKernelSize>() + 1) >> 1) + (filterSize >> 1));
+      (((get<kKernelSize>() + 1) >> 1) + (filterSize >> 1)) + get<kFFT>().winSize();
   }
 
   AnalysisSize analysisSettings() 
