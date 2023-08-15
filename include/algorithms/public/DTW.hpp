fe0d3cf9126817c6a35f26f75d9e97f789f3ba11
lewardo <segfault.ninja@gmail.com>
2023-08-15 09:56:20 +0100

    dtw initial structure

---
+++ include/algorithms/public/DTW.hpp
@@ -0,0 +1,37 @@
+/*
+Part of the Fluid Corpus Manipulation Project (http://www.flucoma.org/)
+Copyright University of Huddersfield.
+Licensed under the BSD-3 License.
+See license.md file in the project root for full license information.
+This project has received funding from the European Research Council (ERC)
+under the European Union’s Horizon 2020 research and innovation programme
+(grant agreement No 725899).
+*/
+
+#pragma once
+
+#include "../util/FluidEigenMappings.hpp"
+#include "../../data/FluidDataSet.hpp"
+#include "../../data/FluidIndex.hpp"
+#include "../../data/FluidTensor.hpp"
+#include "../../data/TensorTypes.hpp"
+#include "../../data/FluidMemory.hpp"
+#include <Eigen/Core>
+#include <random>
+
+namespace fluid {
+namespace algorithm {
+
+class DTW
+{
+public:
+    explicit DTW() = default;
+    ~DTW() = default;
+
+    void init()
+    {
+
+    }
+}
+}
+}
