af564f9dd62a6a92cbe6cdab0476f93b476a8509
lewardo <segfault.ninja@gmail.com>
2023-08-23 11:10:14 +0100

    remove superfluous dataset members and convert names

--- include/clients/nrt/DataSeriesClient.hpp
+++ include/clients/nrt/DataSeriesClient.hpp
@@ -22,7 +22,7 @@ namespace fluid {
 namespace client {
 namespace dataseries {
 
-enum { kName };
+enum { kName, kFrameLen };
 
 constexpr auto DataSeriesParams = defineParameters(
     StringParam<Fixed<true>>("name", "Name of the DataSeries"),
@@ -64,7 +64,7 @@ public:
 
   DataSeriesClient(ParamSetViewType& p, FluidContext&) : mParams(p) {}
 
-  MessageResult<void> addPoint(string id, InputBufferPtr data)
+  MessageResult<void> addFrame(string id, InputBufferPtr data)
   {
     DataSeries& dataset = mAlgorithm;
     if (!data) return Error(NoBuffer);
@@ -79,10 +79,11 @@ public:
       return Error(WrongPointSize);
     RealVector point(dataset.dims());
     point <<= buf.samps(0, dataset.dims(), 0);
-    return dataset.add(id, point) ? OK() : Error(DuplicateIdentifier);
+    dataset.addFrame(id, point);
+    return  OK();
   }
 
-  MessageResult<void> getPoint(string id, BufferPtr data) const
+  MessageResult<void> getFrame(string id, index time, BufferPtr data) const
   {
     if (!data) return Error(NoBuffer);
     BufferAdaptor::Access buf(data.get());
@@ -92,7 +93,7 @@ public:
       return {resizeResult.status(), resizeResult.message()};
     RealVector point(mAlgorithm.dims());
     point <<= buf.samps(0, mAlgorithm.dims(), 0);
-    bool result = mAlgorithm.get(id, point);
+    bool result = mAlgorithm.getFrame(id, time, point);
     if (result)
     {
       buf.samps(0, mAlgorithm.dims(), 0) <<= point;
@@ -104,7 +105,7 @@ public:
     }
   }
 
-  MessageResult<void> updatePoint(string id, InputBufferPtr data)
+  MessageResult<void> updateFrame(string id, index time, InputBufferPtr data)
   {
     if (!data) return Error(NoBuffer);
     BufferAdaptor::ReadAccess buf(data.get());
@@ -112,10 +113,10 @@ public:
     if (buf.numFrames() < mAlgorithm.dims()) return Error(WrongPointSize);
     RealVector point(mAlgorithm.dims());
     point <<= buf.samps(0, mAlgorithm.dims(), 0);
-    return mAlgorithm.update(id, point) ? OK() : Error(PointNotFound);
+    return mAlgorithm.updateFrame(id, time, point) ? OK() : Error(PointNotFound);
   }
 
-  MessageResult<void> setPoint(string id, InputBufferPtr data)
+  MessageResult<void> setFrame(string id, index time, InputBufferPtr data)
   {
     if (!data) return Error(NoBuffer);
 
@@ -125,15 +126,15 @@ public:
       if (buf.numFrames() < mAlgorithm.dims()) return Error(WrongPointSize);
       RealVector point(mAlgorithm.dims());
       point <<= buf.samps(0, mAlgorithm.dims(), 0);
-      bool result = mAlgorithm.update(id, point);
+      bool result = mAlgorithm.updateFrame(id, time, point);
       if (result) return OK();
     }
-    return addPoint(id, data);
+    return addFrame(id, data);
   }
 
-  MessageResult<void> deletePoint(string id)
+  MessageResult<void> deleteFrame(string id, index time)
   {
-    return mAlgorithm.remove(id) ? OK() : Error(PointNotFound);
+    return mAlgorithm.removeFrame(id, time) ? OK() : Error(PointNotFound);
   }
 
   MessageResult<void> merge(SharedClientRef<const DataSeriesClient> dataseriesClient,
@@ -141,65 +142,67 @@ public:
   {
     auto dataseriesClientPtr = dataseriesClient.get().lock();
     if (!dataseriesClientPtr) return Error(NoDataSet);
+
     auto srcDataSeries = dataseriesClientPtr->getDataSeries();
     if (srcDataSeries.size() == 0) return Error(EmptyDataSet);
     if (srcDataSeries.pointSize() != mAlgorithm.pointSize())
       return Error(WrongPointSize);
+
     auto       ids = srcDataSeries.getIds();
-    RealVector point(srcDataSeries.pointSize());
     for (index i = 0; i < srcDataSeries.size(); i++)
     {
-      srcDataSeries.get(ids(i), point);
-      bool added = mAlgorithm.add(ids(i), point);
-      if (!added && overwrite) mAlgorithm.update(ids(i), point);
+      auto series = srcDataSeries.getSeries(ids(i));
+      bool added = mAlgorithm.addSeries(ids(i), series);
+      if (!added && overwrite) mAlgorithm.updateSeries(ids(i), series);
     }
+
     return OK();
   }
 
-  MessageResult<void>
-  fromBuffer(InputBufferPtr data, bool transpose,
-             SharedClientRef<const labelset::LabelSetClient> labels)
-  {
-    if (!data) return Error(NoBuffer);
-    BufferAdaptor::ReadAccess buf(data.get());
-    if (!buf.exists()) return Error(InvalidBuffer);
-    auto bufView = transpose ? buf.allFrames() : buf.allFrames().transpose();
-    if (auto labelsPtr = labels.get().lock())
-    {
-      auto& labelSet = labelsPtr->getLabelSet();
-      if (labelSet.size() != bufView.rows())
-      { return Error("Label set size needs to match the buffer size"); }
-      mAlgorithm = DataSeries(labelSet.getData().col(0),
-                           FluidTensorView<const float, 2>(bufView));
-    }
-    else
-    {
-      algorithm::DataSetIdSequence seq("", 0, 0);
-      FluidTensor<string, 1>       newIds(bufView.rows());
-      seq.generate(newIds);
-      mAlgorithm = DataSeries(newIds, FluidTensorView<const float, 2>(bufView));
-    }
-    return OK();
-  }
+  // MessageResult<void>
+  // fromBuffer(InputBufferPtr data, bool transpose,
+  //            SharedClientRef<const labelset::LabelSetClient> labels)
+  // {
+  //   if (!data) return Error(NoBuffer);
+  //   BufferAdaptor::ReadAccess buf(data.get());
+  //   if (!buf.exists()) return Error(InvalidBuffer);
+  //   auto bufView = transpose ? buf.allFrames() : buf.allFrames().transpose();
+  //   if (auto labelsPtr = labels.get().lock())
+  //   {
+  //     auto& labelSet = labelsPtr->getLabelSet();
+  //     if (labelSet.size() != bufView.rows())
+  //     { return Error("Label set size needs to match the buffer size"); }
+  //     mAlgorithm = DataSeries(labelSet.getData().col(0),
+  //                          FluidTensorView<const float, 2>(bufView));
+  //   }
+  //   else
+  //   {
+  //     algorithm::DataSetIdSequence seq("", 0, 0);
+  //     FluidTensor<string, 1>       newIds(bufView.rows());
+  //     seq.generate(newIds);
+  //     mAlgorithm = DataSeries(newIds, FluidTensorView<const float, 2>(bufView));
+  //   }
+  //   return OK();
+  // }
 
-  MessageResult<void> toBuffer(BufferPtr data, bool transpose,
-                               LabelSetClientRef labels)
-  {
-    if (!data) return Error(NoBuffer);
-    BufferAdaptor::Access buf(data.get());
-    if (!buf.exists()) return Error(InvalidBuffer);
-    index  nFrames = transpose ? mAlgorithm.dims() : mAlgorithm.size();
-    index  nChannels = transpose ? mAlgorithm.size() : mAlgorithm.dims();
-    Result resizeResult = buf.resize(nFrames, nChannels, buf.sampleRate());
-    if (!resizeResult.ok()) return Error(resizeResult.message());
-    buf.allFrames() <<=
-        transpose ? mAlgorithm.getData()
-                  : FluidTensorView<const double, 2>(mAlgorithm.getData())
-                        .transpose();
-    auto labelsPtr = labels.get().lock();
-    if (labelsPtr) labelsPtr->setLabelSet(getIdsLabelSet());
-    return OK();
-  }
+  // MessageResult<void> toBuffer(BufferPtr data, bool transpose,
+  //                              LabelSetClientRef labels)
+  // {
+  //   if (!data) return Error(NoBuffer);
+  //   BufferAdaptor::Access buf(data.get());
+  //   if (!buf.exists()) return Error(InvalidBuffer);
+  //   index  nFrames = transpose ? mAlgorithm.dims() : mAlgorithm.size();
+  //   index  nChannels = transpose ? mAlgorithm.size() : mAlgorithm.dims();
+  //   Result resizeResult = buf.resize(nFrames, nChannels, buf.sampleRate());
+  //   if (!resizeResult.ok()) return Error(resizeResult.message());
+  //   buf.allFrames() <<=
+  //       transpose ? mAlgorithm.getData()
+  //                 : FluidTensorView<const double, 2>(mAlgorithm.getData())
+  //                       .transpose();
+  //   auto labelsPtr = labels.get().lock();
+  //   if (labelsPtr) labelsPtr->setLabelSet(getIdsLabelSet());
+  //   return OK();
+  // }
 
   MessageResult<void> getIds(LabelSetClientRef dest)
   {
@@ -209,47 +212,47 @@ public:
     return OK();
   }
 
-  MessageResult<FluidTensor<rt::string, 1>> kNearest(InputBufferPtr data,
-                                                     index nNeighbours) const
-  {
-    // check for nNeighbours > 0 and < size of DS
-    if (nNeighbours > mAlgorithm.size())
-      return Error<FluidTensor<rt::string, 1>>(SmallDataSet);
-    if (nNeighbours <= 0) return Error<FluidTensor<rt::string, 1>>(SmallK);
+  // MessageResult<FluidTensor<rt::string, 1>> kNearest(InputBufferPtr data,
+  //                                                    index nNeighbours) const
+  // {
+  //   // check for nNeighbours > 0 and < size of DS
+  //   if (nNeighbours > mAlgorithm.size())
+  //     return Error<FluidTensor<rt::string, 1>>(SmallDataSet);
+  //   if (nNeighbours <= 0) return Error<FluidTensor<rt::string, 1>>(SmallK);
 
-    InBufferCheck bufCheck(mAlgorithm.dims());
+  //   InBufferCheck bufCheck(mAlgorithm.dims());
 
-    if (!bufCheck.checkInputs(data.get()))
-      return Error<FluidTensor<rt::string, 1>>(bufCheck.error());
+  //   if (!bufCheck.checkInputs(data.get()))
+  //     return Error<FluidTensor<rt::string, 1>>(bufCheck.error());
 
-    FluidTensor<const double, 1> point(
-        BufferAdaptor::ReadAccess(data.get()).samps(0, mAlgorithm.dims(), 0));
+  //   FluidTensor<const double, 1> point(
+  //       BufferAdaptor::ReadAccess(data.get()).samps(0, mAlgorithm.dims(), 0));
 
-    std::vector<index> indices(asUnsigned(mAlgorithm.size()));
-    std::iota(indices.begin(), indices.end(), 0);
-    std::vector<double> distances(asUnsigned(mAlgorithm.size()));
+  //   std::vector<index> indices(asUnsigned(mAlgorithm.size()));
+  //   std::iota(indices.begin(), indices.end(), 0);
+  //   std::vector<double> distances(asUnsigned(mAlgorithm.size()));
 
-    auto ds = mAlgorithm.getData();
+  //   auto ds = mAlgorithm.getData();
 
-    std::transform(
-        indices.begin(), indices.end(), distances.begin(),
-        [&point, &ds, this](index i) { return distance(point, ds.row(i)); });
+  //   std::transform(
+  //       indices.begin(), indices.end(), distances.begin(),
+  //       [&point, &ds, this](index i) { return distance(point, ds.row(i)); });
 
-    std::sort(indices.begin(), indices.end(), [&distances](index a, index b) {
-      return distances[asUnsigned(a)] < distances[asUnsigned(b)];
-    });
+  //   std::sort(indices.begin(), indices.end(), [&distances](index a, index b) {
+  //     return distances[asUnsigned(a)] < distances[asUnsigned(b)];
+  //   });
 
-    FluidTensor<rt::string, 1> labels(nNeighbours);
+  //   FluidTensor<rt::string, 1> labels(nNeighbours);
 
-    std::transform(
-        indices.begin(), indices.begin() + nNeighbours, labels.begin(),
-        [this](index i) {
-          std::string const& id = mAlgorithm.getIds()[i];
-          return rt::string{id, 0, id.size(), FluidDefaultAllocator()};
-        });
+  //   std::transform(
+  //       indices.begin(), indices.begin() + nNeighbours, labels.begin(),
+  //       [this](index i) {
+  //         std::string const& id = mAlgorithm.getIds()[i];
+  //         return rt::string{id, 0, id.size(), FluidDefaultAllocator()};
+  //       });
 
-    return labels;
-  }
+  //   return labels;
+  // }
 
   MessageResult<void> clear()
   {
@@ -268,11 +271,11 @@ public:
   static auto getMessageDescriptors()
   {
     return defineMessages(
-        makeMessage("addPoint",     &DataSeriesClient::addPoint),
-        makeMessage("getPoint",     &DataSeriesClient::getPoint),
-        makeMessage("setPoint",     &DataSeriesClient::setPoint),
-        makeMessage("updatePoint",  &DataSeriesClient::updatePoint),
-        makeMessage("deletePoint",  &DataSeriesClient::deletePoint),
+        makeMessage("addPoint",     &DataSeriesClient::addFrame),
+        makeMessage("getPoint",     &DataSeriesClient::getFrame),
+        makeMessage("setPoint",     &DataSeriesClient::setFrame),
+        makeMessage("updatePoint",  &DataSeriesClient::updateFrame),
+        makeMessage("deletePoint",  &DataSeriesClient::deleteFrame),
         makeMessage("merge",        &DataSeriesClient::merge),
         makeMessage("dump",         &DataSeriesClient::dump),
         makeMessage("load",         &DataSeriesClient::load),
@@ -282,10 +285,11 @@ public:
         makeMessage("clear",        &DataSeriesClient::clear),
         makeMessage("write",        &DataSeriesClient::write),
         makeMessage("read",         &DataSeriesClient::read),
-        makeMessage("fromBuffer",   &DataSeriesClient::fromBuffer),
-        makeMessage("toBuffer",     &DataSeriesClient::toBuffer),
-        makeMessage("getIds",       &DataSeriesClient::getIds),
-        makeMessage("kNearest",     &DataSeriesClient::kNearest));
+        // makeMessage("fromBuffer",   &DataSeriesClient::fromBuffer),
+        // makeMessage("toBuffer",     &DataSeriesClient::toBuffer),
+        makeMessage("getIds",       &DataSeriesClient::getIds)
+        // makeMessage("kNearest",     &DataSeriesClient::kNearest)
+    );
   }
 
 private:
@@ -299,12 +303,12 @@ private:
     return LabelSet(newIds, labels);
   };
   
-  double distance(FluidTensorView<const double, 1> point1, FluidTensorView<const double, 1> point2) const
-  {    
-    return std::transform_reduce(point1.begin(), point1.end(), point2.begin(), 0.0, std::plus{}, [](double v1, double v2){
-      return (v1-v2) * (v1-v2);
-    });
-  };
+  // double distance(FluidTensorView<const double, 1> point1, FluidTensorView<const double, 1> point2) const
+  // {    
+  //   return std::transform_reduce(point1.begin(), point1.end(), point2.begin(), 0.0, std::plus{}, [](double v1, double v2){
+  //     return (v1-v2) * (v1-v2);
+  //   });
+  // };
 };
 
 } // namespace dataset
