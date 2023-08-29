beb0ff16e048740516a22b419a92efa2c0b3cceb
lewardo <segfault.ninja@gmail.com>
2023-08-29 15:44:39 +0100

    added Constraint object

--- include/algorithms/public/DTW.hpp
+++ include/algorithms/public/DTW.hpp
@@ -25,12 +25,16 @@ namespace fluid {
 namespace algorithm {
 
 
+enum class DTWConstraint { kUnconstrained, kIkatura, kSakoeChiba };
+
 // debt of gratitude to the wonderful article on
 // https://rtavenar.github.io/blog/dtw.html a better explanation of DTW than any
 // other algorithm explanation I've seen
 
 class DTW
 {
+  class Constraint;
+
 public:
   explicit DTW() = default;
   ~DTW() = default;
@@ -43,14 +47,48 @@ public:
   constexpr index initialized() const { return mInitialized; }
 
   double process(InputRealMatrixView x1, InputRealMatrixView x2,
-                 Allocator& alloc = FluidDefaultAllocator())
+                 DTWConstraint c = DTWConstraint::kUnconstrained,
+                 Allocator&    alloc = FluidDefaultAllocator())
   {
-    return calculateDistanceMetrics(x1, x2, x1, alloc);
+    Constraint                      constraint(c, x1.rows(), x2.rows());
+    ScopedEigenMap<Eigen::VectorXd> x1r(x1.cols(), alloc),
+        x2r(x2.cols(), alloc);
+
+    mDistanceMetrics.resize(x1.rows(), x2.rows());
+    mDistanceMetrics.fill(std::numeric_limits<double>::max());
+
+    // simple brute force DTW is very inefficient, see FastDTW
+    for (index i = 0; i < x1.rows(); i++)
+    {
+      for (index j = 0; j < x2.rows(); j++)
+      {
+        x1r = _impl::asEigen<Eigen::Matrix>(x1.row(i));
+        x2r = _impl::asEigen<Eigen::Matrix>(x2.row(j));
+
+        mDistanceMetrics(i, j) = differencePNormToTheP(x1r, x2r);
+
+        if (i > 0 || j > 0)
+        {
+          double minimum = std::numeric_limits<double>::max();
+
+          if (i > 0) minimum = std::min(minimum, mDistanceMetrics(i - 1, j));
+          if (j > 0) minimum = std::min(minimum, mDistanceMetrics(i, j - 1));
+          if (i > 0 && j > 0)
+            minimum = std::min(minimum, mDistanceMetrics(i - 1, j - 1));
+
+          mDistanceMetrics(i, j) += minimum;
+        }
+      }
+    }
+
+    mCalculated = true;
+
+    return std::pow(mDistanceMetrics(x1.rows() - 1, x2.rows() - 1),
+                    1.0 / mPNorm);
   }
 
 private:
   RealMatrix mDistanceMetrics;
-  RealMatrix mPath;
   index      mPNorm{2};
 
   bool       mCalculated{false};
@@ -71,45 +109,46 @@ private:
     return (v1.array() - v2.array()).abs().pow(mPNorm).sum();
   }
 
-  double calculateDistanceMetrics(InputRealMatrixView x1,
-                                  InputRealMatrixView x2,
-                                  InputRealMatrixView window,
-                                  Allocator& alloc = FluidDefaultAllocator())
+  class Constraint
   {
-    ScopedEigenMap<Eigen::VectorXd> x1r(x1.cols(), alloc),
-        x2r(x2.cols(), alloc);
+    Constraint(DTWConstraint c, index rows, index cols)
+        : mType{c}, mRows{rows}, mCols{cols} {};
 
-    mDistanceMetrics.resize(x1.rows(), x2.rows());
+    const index rowStart() const { return 0; };
+    const index rowEnd() const { return mRows; };
 
-    // simple brute force DTW is very inefficient, see FastDTW
-    for (index i = 0; i < x1.rows(); i++)
+    index colStart(index row)
     {
-      for (index j = 0; j < x2.rows(); j++)
+      switch (mType)
       {
-        x1r = _impl::asEigen<Eigen::Matrix>(x1.row(i));
-        x2r = _impl::asEigen<Eigen::Matrix>(x2.row(j));
+      case DTWConstraint::kUnconstrained: return 0;
 
-        mDistanceMetrics(i, j) = differencePNormToTheP(x1r, x2r);
+      case DTWConstraint::kIkatura: break;
 
-        if (i > 0 || j > 0)
-        {
-          double minimum = std::numeric_limits<double>::max();
+      case DTWConstraint::kSakoeChiba: break;
 
-          if (i > 0 && j > 0)
-            minimum = std::min(minimum, mDistanceMetrics(i - 1, j - 1));
-          if (i > 0) minimum = std::min(minimum, mDistanceMetrics(i - 1, j));
-          if (j > 0) minimum = std::min(minimum, mDistanceMetrics(i, j - 1));
-
-          mDistanceMetrics(i, j) += minimum;
-        }
+      default: return -1;
       }
-    }
+    };
 
-    mCalculated = true;
+    index colEnd()
+    {
+      switch (mType)
+      {
+      case DTWConstraint::kUnconstrained: return mCols;
 
-    return std::pow(mDistanceMetrics(x1.rows() - 1, x2.rows() - 1),
-                    1.0 / mPNorm);
-  }
+      case DTWConstraint::kIkatura: break;
+
+      case DTWConstraint::kSakoeChiba: break;
+
+      default: return -1;
+      }
+    };
+
+  private:
+    DTWConstraint mType;
+    index         mRows, mCols;
+  };
 };
 
 } // namespace algorithm
