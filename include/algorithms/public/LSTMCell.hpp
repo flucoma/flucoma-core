cc7b6a39479c24ccb09c06447cdeb1170a769e5e
lewardo <segfault.ninja@gmail.com>
2023-09-01 12:57:13 +0100

    both array and matrix views for fun

--- include/algorithms/public/LSTMCell.hpp
+++ include/algorithms/public/LSTMCell.hpp
@@ -19,6 +19,23 @@ under the European Union’s Horizon 2020 research and innovation programme
 #include <Eigen/Core>
 #include <random>
 
+// MATRIX WIGHT NAMING SYSTEM BECAUSE IT IS VERY SUCCINCT SINCE I DONT WANT
+// REALLY LONG LINES
+// regex: m(E[MA])?D?[WB][igfo]
+//
+// m prefix => code style, member of class
+// E => not real matrix, is a view (Eigen::Map)
+// [MA] => eigen matrix view and eigen array view, respectively
+// D => derivative of relevant matrix
+// [WB] => weight matrix or bias vector
+// [igfo] => what the weight is for: input gate, input weighting, forget gate,
+//           output gate, respectively
+//
+// e.g. mWi -> input gate weights
+//      mDBo -> derivative of output gate biases
+//      mEMWf -> eigen matrix map of the forget gate weights
+//      mEADBf -> eigen array map of the derivative of the forget gate biases
+
 namespace fluid {
 namespace algorithm {
 
@@ -30,10 +47,12 @@ class LSTMParam
   using VectorXd = Eigen::VectorXd;
   using MatrixXd = Eigen::MatrixXd;
   using ArrayXd = Eigen::ArrayXd;
+  using ArrayXXd = Eigen::ArrayXXd;
 
   using EigenMatrixMap = Eigen::Map<MatrixXd>;
   using EigenVectorMap = Eigen::Map<VectorXd>;
-  using EigenArrayMap = Eigen::Map<ArrayXd>;
+  using EigenArrayXMap = Eigen::Map<ArrayXd>;
+  using EigenArrayXXMap = Eigen::Map<ArrayXXd>;
 
 public:
   LSTMParam(index inputSize, index outputSize)
@@ -54,25 +73,35 @@ public:
         // allocate the memory for the bias derivatives
         mDBi(mOutSize), mDBg(mOutSize), mDBf(mOutSize), mDBo(mOutSize),
 
-        // create eigen maps for the weights
-        mEWi(mWi.data(), mWi.rows(), mWi.cols()),
-        mEWg(mWg.data(), mWg.rows(), mWg.cols()),
-        mEWf(mWf.data(), mWf.rows(), mWf.cols()),
-        mEWo(mWo.data(), mWo.rows(), mWo.cols()),
+        // create matrix eigen maps for the weights
+        mEMWi(mWi.data(), mWi.rows(), mWi.cols()),
+        mEMWg(mWg.data(), mWg.rows(), mWg.cols()),
+        mEMWf(mWf.data(), mWf.rows(), mWf.cols()),
+        mEMWo(mWo.data(), mWo.rows(), mWo.cols()),
 
-        // create eigen maps for the wight derivatives
-        mEDWi(mDWi.data(), mDWi.rows(), mDWi.cols()),
-        mEDWg(mDWg.data(), mDWg.rows(), mDWg.cols()),
-        mEDWf(mDWf.data(), mDWf.rows(), mDWf.cols()),
-        mEDWo(mDWo.data(), mDWo.rows(), mDWo.cols()),
+        // create matrix eigen maps for the biases
+        mEMBi(mBi.data(), mBi.size()), mEMBg(mBg.data(), mBg.size()),
+        mEMBf(mBf.data(), mBf.size()), mEMBo(mBo.data(), mBo.size()),
 
-        // create eigen maps for the biases
-        mEBi(mBi.data(), mBi.size()), mEBg(mBg.data(), mBg.size()),
-        mEBf(mBf.data(), mBf.size()), mEBo(mBo.data(), mBo.size()),
+        // create array eigen maps for the weights
+        mEAWi(mWi.data(), mWi.rows(), mWi.cols()),
+        mEAWg(mWg.data(), mWg.rows(), mWg.cols()),
+        mEAWf(mWf.data(), mWf.rows(), mWf.cols()),
+        mEAWo(mWo.data(), mWo.rows(), mWo.cols()),
 
-        // create eigen maps for the bias derivatives
-        mEDBi(mDBi.data(), mDBi.size()), mEDBg(mDBg.data(), mDBg.size()),
-        mEDBf(mDBf.data(), mDBf.size()), mEDBo(mDBo.data(), mDBo.size())
+        // create array eigen maps for the biases
+        mEABi(mBi.data(), mBi.size()), mEABg(mBg.data(), mBg.size()),
+        mEABf(mBf.data(), mBf.size()), mEABo(mBo.data(), mBo.size()),
+
+        // create array eigen maps for the weight derivatives
+        mEADWi(mDWi.data(), mDWi.rows(), mDWi.cols()),
+        mEADWg(mDWg.data(), mDWg.rows(), mDWg.cols()),
+        mEADWf(mDWf.data(), mDWf.rows(), mDWf.cols()),
+        mEADWo(mDWo.data(), mDWo.rows(), mDWo.cols()),
+
+        // create array eigen maps for the bias derivatives
+        mEADBi(mDBi.data(), mDBi.size()), mEADBg(mDBg.data(), mDBg.size()),
+        mEADBf(mDBf.data(), mDBf.size()), mEADBo(mDBo.data(), mDBo.size())
   {
     std::random_device rnd_device;
     std::mt19937       mersenne_engine{rnd_device()};
@@ -93,15 +122,15 @@ public:
 
   void apply(double lr)
   {
-    mEWi -= lr * mEDWi;
-    mEWg -= lr * mEDWg;
-    mEWf -= lr * mEDWf;
-    mEWo -= lr * mEDWo;
+    mEAWi -= lr * mEADWi;
+    mEAWg -= lr * mEADWg;
+    mEAWf -= lr * mEADWf;
+    mEAWo -= lr * mEADWo;
 
-    mEBi -= lr * mEDBi;
-    mEBg -= lr * mEDBg;
-    mEBf -= lr * mEDBf;
-    mEBo -= lr * mEDBo;
+    mEABi -= lr * mEADBi;
+    mEABg -= lr * mEADBg;
+    mEABf -= lr * mEADBf;
+    mEABo -= lr * mEADBo;
 
     // clear weight derivatives
     mDWi.fill(0.0);
@@ -124,41 +153,50 @@ public:
   RealVector mBi, mBg, mBf, mBo;
   RealVector mDBi, mDBg, mDBf, mDBo;
 
-  // eigen maps to the parameters
-  EigenMatrixMap mEWi, mEWg, mEWf, mEWo;
-  EigenMatrixMap mEDWi, mEDWg, mEDWf, mEDWo;
-  EigenVectorMap mEBi, mEBg, mEBf, mEBo;
-  EigenVectorMap mEDBi, mEDBg, mEDBf, mEDBo;
+  // eigen maps to the parameters for linear algebra
+  EigenMatrixMap mEMWi, mEMWg, mEMWf, mEMWo;
+  EigenVectorMap mEMBi, mEMBg, mEMBf, mEMBo;
+
+  // eigen maps to the parameters for element-wise algebra
+  EigenArrayXXMap mEAWi, mEAWg, mEAWf, mEAWo;
+  EigenArrayXXMap mEADWi, mEADWg, mEADWf, mEADWo;
+  EigenArrayXMap  mEABi, mEABg, mEABf, mEABo;
+  EigenArrayXMap  mEADBi, mEADBg, mEADBf, mEADBo;
 };
 
 class LSTMState
 {
   using VectorXd = Eigen::VectorXd;
-  using MatrixXd = Eigen::MatrixXd;
   using ArrayXd = Eigen::ArrayXd;
 
-  using EigenMatrixMap = Eigen::Map<MatrixXd>;
   using EigenVectorMap = Eigen::Map<VectorXd>;
-  using EigenArrayMap = Eigen::Map<ArrayXd>;
+  using EigenArrayXMap = Eigen::Map<ArrayXd>;
 
 public:
   LSTMState(index inputSize, index outputSize)
       : mI(outputSize), mG(outputSize), mF(outputSize), mO(outputSize),
         mC(outputSize), mH(outputSize), mDC(outputSize), mDH(outputSize),
 
-        mEI(mI.data(), mI.size()), mEG(mG.data(), mG.size()),
-        mEF(mF.data(), mF.size()), mEO(mO.data(), mO.size()),
-        mEC(mC.data(), mC.size()), mEH(mH.data(), mH.size()),
-        mEDC(mDC.data(), mDC.size()), mEDH(mDH.data(), mDH.size())
+        mEMI(mI.data(), mI.size()), mEMG(mG.data(), mG.size()),
+        mEMF(mF.data(), mF.size()), mEMO(mO.data(), mO.size()),
+        mEMC(mC.data(), mC.size()), mEMH(mH.data(), mH.size()),
+
+        mEAI(mI.data(), mI.size()), mEAG(mG.data(), mG.size()),
+        mEAF(mF.data(), mF.size()), mEAO(mO.data(), mO.size()),
+        mEAC(mC.data(), mC.size()), mEAH(mH.data(), mH.size()),
+        mEADC(mDC.data(), mDC.size()), mEADH(mDH.data(), mDH.size())
   {}
 
   // state at time t
   RealVector mI, mG, mF, mO, mC, mH;
   RealVector mDC, mDH;
 
-  // eigen maps to the states for schmancy maths
-  EigenVectorMap mEI, mEG, mEF, mEO, mEC, mEH;
-  EigenVectorMap mEDC, mEDH;
+  // eigen maps to the states for linear algebra
+  EigenVectorMap mEMI, mEMG, mEMF, mEMO, mEMC, mEMH;
+
+  // eigen maps to the states for element-wise algebra
+  EigenArrayXMap mEAI, mEAG, mEAF, mEAO, mEAC, mEAH;
+  EigenArrayXMap mEADC, mEADH;
 };
 
 class LSTMCell
