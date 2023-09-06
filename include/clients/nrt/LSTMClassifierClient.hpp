c5e3f59166d61fb107be6a5815c9e627513fa8d3
lewardo <segfault.ninja@gmail.com>
2023-09-06 15:20:39 +0100

    initial classifier structure

---
+++ include/clients/nrt/LSTMClassifierClient.hpp
@@ -0,0 +1,101 @@
+/*
+Part of the Fluid Corpus Manipulation Project (http://www.flucoma.org/)
+Copyright University of Huddersfield.
+Licensed under the BSD-3 License.
+See license.md file in the project root for full license information.
+This project has received funding from the European Research Council (ERC)
+under the European Union’s Horizon 2020 research and innovation programme
+(grant agreement No 725899).
+*/
+
+#pragma once
+
+#include "DataClient.hpp"
+#include "DataSeriesClient.hpp"
+#include "DataSetClient.hpp"
+#include "NRTClient.hpp"
+#include "../../algorithms/public/LSTM.hpp"
+#include "../../algorithms/public/Recur.hpp"
+#include <string>
+
+namespace fluid {
+namespace client {
+namespace lstmclassifier {
+
+constexpr auto LSTMClassifierParams = defineParameters(
+    StringParam<Fixed<true>>("name", "Name"),
+    LongParam("maxIter", "Maximum Number of Iterations", 1000, Min(1)),
+    FloatParam("learnRate", "Learning Rate", 0.01, Min(0.0), Max(1.0)),
+    LongParam("batchSize", "Batch Size", 50, Min(1)));
+
+class LSTMClassifierClient
+    : public FluidBaseClient,
+      OfflineIn,
+      OfflineOut,
+      ModelObject,
+      public DataClient<algorithm::Recur<algorithm::LSTMCell>>
+{
+  enum { kName, kMaxIter, kLearnRate, kBatchSize, kValidation };
+
+public:
+  using string = std::string;
+  using BufferPtr = std::shared_ptr<BufferAdaptor>;
+  using InputBufferPtr = std::shared_ptr<const BufferAdaptor>;
+  using StringVector = FluidTensor<rt::string, 1>;
+
+  using ParamDescType = decltype(LSTMClassifierParams);
+  using ParamSetViewType = ParameterSetView<ParamDescType>;
+  using ParamValues = typename ParamSetViewType::ValueTuple;
+
+  std::reference_wrapper<ParamSetViewType> mParams;
+
+  void setParams(ParamSetViewType& p) { mParams = p; }
+
+  template <size_t N>
+  auto& get() const
+  {
+    return mParams.get().template get<N>();
+  }
+
+  static constexpr auto& getParameterDescriptors()
+  {
+    return LSTMClassifierParams;
+  }
+
+  LSTMClassifierClient(ParamSetViewType& p, FluidContext&) : mParams(p)
+  {
+    controlChannelsIn(1);
+    controlChannelsOut({1, 1});
+  }
+
+  template <typename T>
+  Result process(FluidContext&)
+  {
+    return {};
+  }
+
+  MessageResult<double> fit(InputDataSeriesClientRef dataseriesClient) {}
+  MessageResult<double> predict(InputDataSeriesClientRef dataseriesClient) {}
+  MessageResult<double> predictPoint(InputDataSeriesClientRef dataseriesClient)
+  {}
+
+  static auto getMessageDescriptors()
+  {
+    return defineMessages(
+        makeMessage("fit", &LSTMClassifierClient::fit),
+        makeMessage("predict", &LSTMClassifierClient::predict),
+        makeMessage("predictPoint", &LSTMClassifierClient::predictPoint));
+  }
+
+private:
+};
+
+using LSTMClassifierRef = SharedClientRef<const LSTMClassifierClient>;
+
+} // namespace lstmclassifier
+
+using NRTThreadedDTWClient =
+    NRTThreadingAdaptor<typename lstmclassifier::LSTMClassifierRef::SharedType>;
+
+} // namespace client
+} // namespace fluid
