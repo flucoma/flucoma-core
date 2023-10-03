579f6c9f3d9cedb51ed91787bffdd8b12904a88c
lewardo <segfault.ninja@gmail.com>
2023-10-03 12:25:46 +0100

    added validation to training

--- include/algorithms/public/RecurSGD.hpp
+++ include/algorithms/public/RecurSGD.hpp
@@ -36,53 +36,31 @@ public:
 
   double trainManyToOne(Recur<CellType>& model, InputDataSeriesView in,
                         InputRealMatrixView out, index nIter, index batchSize,
-                        double learningRate, double maxCost = 100)
+                        double learningRate, double validation = 0.1)
   {
+    assert(in.size() == out.rows());
     assert(model.outputDims() == out.cols());
     assert(in.end() == std::find_if(in.begin(), in.end(), [&model](auto& x) {
              return model.inputDims() != x.cols();
            }));
 
-    rt::vector<index> permutation(in.size());
-    std::iota(permutation.begin(), permutation.end(), 0);
+    double error = trainSGD(
+        model, in.size(), nIter, batchSize, learningRate,
+        validation, [&in, &out, &model](index i) -> double {
+          assert(i >= 0 && i < in.size());
+          assert(i >= 0 && i < out.rows());
 
-    double error;
-    index  normalise;
+          return model.fit(in[i], out.row(i));
+        });
 
-    while (nIter-- > 0)
-    {
-      error = 0.0;
-      normalise = 0;
-
-      std::shuffle(permutation.begin(), permutation.end(),
-                   std::mt19937{std::random_device{}()});
-
-      for (index batchStart = 0; asUnsigned(batchStart) < in.size();
-           batchStart += batchSize)
-      {
-        index thisBatchSize = asUnsigned(batchStart + batchSize) < in.size()
-                                  ? batchSize
-                                  : in.size() - batchStart;
-
-        model.reset();
-        for (index i = batchStart; i < batchStart + thisBatchSize; ++i)
-          error += model.fit(in[permutation[i]], out.row(permutation[i]));
-
-        model.update(learningRate);
-        normalise += thisBatchSize;
-      }
-
-      if (error > maxCost) model.clear();
-    }
-
-    model.setTrained(true);
-    return error / normalise;
+    return error;
   }
 
   double trainManyToMany(Recur<CellType>& model, InputDataSeriesView in,
                          InputDataSeriesView out, index nIter, index batchSize,
-                         double learningRate, double maxCost = 100)
+                         double learningRate, double validation = 0.1)
   {
+    assert(in.size() == out.size());
     assert(in.end() == std::find_if(in.begin(), in.end(), [&model](auto& x) {
              return model.inputDims() != x.cols();
            }));
@@ -90,46 +68,21 @@ public:
              return model.outputDims() != x.cols();
            }));
 
-    rt::vector<index> permutation(in.size());
-    std::iota(permutation.begin(), permutation.end(), 0);
+    double error = trainSGD(model, in.size(), nIter, batchSize, learningRate,
+                            validation, [&in, &out, &model](index i) -> double {
+                              assert(i >= 0 && i < in.size());
+                              assert(i >= 0 && i < out.size());
 
-    double error;
-    index  normalise;
+                              return model.fit(in[i], out[i]);
+                            });
 
-    while (nIter-- > 0)
-    {
-      error = 0.0;
-      normalise = 0;
-
-      std::shuffle(permutation.begin(), permutation.end(),
-                   std::mt19937{std::random_device{}()});
-
-      for (index batchStart = 0; batchStart < in.size();
-           batchStart += batchSize)
-      {
-        index thisBatchSize = (batchStart + batchSize) < in.size()
-                                  ? batchSize
-                                  : in.size() - batchStart;
-
-        model.reset();
-        for (index i = batchStart; i < batchStart + thisBatchSize; ++i)
-          error += model.fit(in[permutation[i]], out[permutation[i]]);
-
-        model.update(learningRate);
-        normalise += thisBatchSize;
-      }
-
-      if (error > maxCost) model.clear();
-    }
-
-    model.setTrained(true);
-    return error / normalise;
+    return error;
   }
 
 
   double trainPredictor(Recur<CellType>& model, InputDataSeriesView data,
                         index nIter, index batchSize, double learningRate,
-                        double maxCost = 100)
+                        double validation = 0.1)
   {
     assert(model.inputDims() == model.outputDims());
     assert(data.end() ==
@@ -137,40 +90,70 @@ public:
              return model.dims() != x.cols();
            }));
 
-    rt::vector<index> permutation(data.size());
+    double error = trainSGD(model, data.size(), nIter, batchSize, learningRate,
+                            validation, [&data, &model](index i) -> double {
+                              assert(i >= 0 && i < data.size());
+                              return model.fit(data[i]);
+                            });
+
+    return error;
+  }
+
+private:
+  double trainSGD(Recur<CellType>& model, index corpusSize, index nIter,
+                  index batchSize, double learningRate, double validation,
+                  std::function<double(index)> fit)
+  {
+    rt::vector<index> permutation(corpusSize);
     std::iota(permutation.begin(), permutation.end(), 0);
 
-    double error;
-    index  normalise;
+    double error = 0.0, prevError = 0.0;
 
-    while (nIter-- > 0)
+    index patience = 10;
+    index nValidate = validation * corpusSize;
+    index nTrain = corpusSize - nValidate;
+
+    while (nIter-- > 0 && patience > 0)
     {
       error = 0.0;
-      normalise = 0;
 
       std::shuffle(permutation.begin(), permutation.end(),
                    std::mt19937{std::random_device{}()});
 
-      for (index batchStart = 0; batchStart < data.size();
+      for (index batchStart = 0; batchStart < nTrain;
            batchStart += batchSize)
       {
-        index thisBatchSize = (batchStart + batchSize) < data.size()
+        index thisBatchSize = (batchStart + batchSize) < nTrain
                                   ? batchSize
-                                  : data.size() - batchStart;
+                                  : nTrain - batchStart;
 
         model.reset();
         for (index i = batchStart; i < batchStart + thisBatchSize; ++i)
-          error += model.fit(data[permutation[i]]);
+          error += fit(permutation[i]);
 
         model.update(learningRate);
-        normalise += thisBatchSize;
       }
 
-      if (error > maxCost) model.clear();
+      for (index i = nTrain; i < corpusSize; ++i)
+      {
+        error += fit(permutation[i]);
+      }
+
+      if (error > prevError)
+      {
+        prevError = error;
+        --patience;
+      }
     }
 
     model.setTrained(true);
-    return error / normalise;
+
+    for (index i = 0; i < corpusSize; ++i)
+    {
+      error += fit(i);
+    }
+
+    return error / corpusSize;
   }
 };
 
