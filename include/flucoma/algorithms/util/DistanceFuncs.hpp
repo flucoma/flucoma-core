901368aec0a581aa25430fa32dc07038fc4167f9
Owen Green <gungwho@gmail.com>
2025-07-14 15:13:30 +0100

    DistanceFuncs: Missing include

--- include/flucoma/algorithms/util/DistanceFuncs.hpp
+++ include/flucoma/algorithms/util/DistanceFuncs.hpp
@@ -11,6 +11,7 @@ under the European Union’s Horizon 2020 research and innovation programme
 #pragma once
 
 #include "AlgorithmUtils.hpp"
+#include "../../data/FluidIndex.hpp"
 #include <Eigen/Core>
 #include <cassert>
 #include <cmath>
