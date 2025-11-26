db9d18052bf1428f15e6f2c2698f65371d37a4ff
Pierre Alexandre Tremblay <info@pierrealexandretremblay.com>
2025-11-26 17:38:07 +0100

    argghhhhh griffinlim in line with difficult i/o

---
+++ tests/algorithms/public/GriffinLim.cpp
@@ -0,0 +1,34 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch_all.hpp>
+#include <flucoma/algorithms/public/GriffinLim.hpp>
+#include <flucoma/data/FluidTensor.hpp>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+TEST_CASE("GriffinLim is repeatable with user-supplied random seed")
+{
+
+  using fluid::algorithm::NMFCross;
+  using Tensor = fluid::FluidTensor<double, 2>;
+  GriffinLim algo();
+
+  Tensor complexIn{{0.5, 0.4}, {0.1, 1.1}, {0.7, 0.8}, {0.3, 0.0}, {1.0, 0.9}, {0.2, 0.6}};
+
+  std::vector Hs(3, Tensor(6, 6));
+
+  algo.process(Hs[0], sourceMag, 3, 2, 7, 42); //arrrrhhhh
+  algo.process(Hs[1], sourceMag, 3, 2, 7, 42);
+  algo.process(Hs[2], sourceMag, 3, 2, 7, 5063);
+
+  using Catch::Matchers::RangeEquals;
+
+  SECTION("Calls with the same seed have the same output")
+  {
+    REQUIRE_THAT(Hs[1], RangeEquals(Hs[0]));
+  }
+  SECTION("Calls with different seeds have different outputs")
+  {
+    REQUIRE_THAT(Hs[1], !RangeEquals(Hs[2]));
+  }
+}
